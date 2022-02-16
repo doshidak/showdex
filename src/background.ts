646204fb2e92a52f9a4eea2ae5954b44de8208c5
Keith Choison <keith.c@live.co.kr>
2022-02-16 15:51:04 -0800

    feat: forgot to commit bg for runtimeFetch

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,5 @@
-chrome.runtime.onMessage.addListener((message, sender) => {
-  switch ((<Record<string, string>> message).type) {
+chrome.runtime.onMessage.addListener((message: Record<string, unknown>, sender) => {
+  switch (<string> message?.type) {
     case 'showPageAction': {
       chrome.pageAction.show(sender.tab.id);
 
@@ -10,6 +10,48 @@ chrome.runtime.onMessage.addListener((message, sender) => {
       chrome.pageAction.hide(sender.tab.id);
 
       break;
+    }
+
+    default: break;
+  }
+});
+
+chrome.runtime.onMessageExternal.addListener((message: Record<string, unknown>, _sender, sendResponse) => {
+  switch (<string> message?.type) {
+    case 'fetch': {
+      void (async () => {
+        try {
+          const response = await fetch(<string> message?.url, {
+            method: 'GET',
+            ...(<Record<string, unknown>> message?.options),
+            headers: {
+              Accept: 'application/json',
+              ...(<Record<string, unknown>> (<Record<string, unknown>> message?.options)?.headers),
+            },
+          });
+
+          const json = await <Promise<Record<string, unknown>>> response.json();
+
+          sendResponse({
+            ok: response.ok,
+            status: response.status,
+            jsonValue: json,
+          });
+        } catch (error) {
+          sendResponse(error);
+        }
+      })();
+
+      // fetch(<string> message?.url, {
+      //   method: 'GET',
+      //   ...(<Record<string, unknown>> message?.options),
+      //   headers: {
+      //     Accept: 'application/json',
+      //     ...(<Record<string, unknown>> (<Record<string, unknown>> message?.options)?.headers),
+      //   },
+      // }).then((response) => sendResponse(response)).catch((error) => sendResponse(error));
+
+      return true; // use `sendResponse` asynchronously
     }
 
     default: break;
