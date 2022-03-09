06dd4670f308d6c4cede5d03ee5be842fee0a7d1
Keith Choison <keith.c@live.co.kr>
2022-03-08 22:59:32 -0800

    feat(utils): added dashes to printBuildInfo

--- src/utils/debug/printBuildInfo.ts
+++ src/utils/debug/printBuildInfo.ts
@@ -1,5 +1,6 @@
 export const printBuildInfo = (): string => (
-  `${process.env.PACKAGE_NAME} v${process.env.PACKAGE_VERSION} ` +
-  `b${process.env.PACKAGE_BUILD_DATE}` +
-  `-${__DEV__ ? 'dev' : 'prod'}`
+  process.env.PACKAGE_NAME +
+  `-v${process.env.PACKAGE_VERSION}` +
+  `-b${process.env.PACKAGE_BUILD_DATE}` +
+  `${__DEV__ ? '-dev' : ''}`
 );
