91c700b539258184b2dc8dc9360b8817ce720c06
Keith Choison <keith.c@live.co.kr>
2022-03-10 15:52:22 -0800

    style(consts): made stats consts look prettier

--- src/consts/stats.ts
+++ src/consts/stats.ts
@@ -1,2 +1,20 @@
-export const PokemonStatNames: Showdown.StatName[] = ['hp', 'atk', 'def', 'spa', 'spd', 'spe'];
-export const PokemonBoostNames: Showdown.StatNameNoHp[] = ['atk', 'def', 'spa', 'spd', 'spe'];
+export const PokemonStatNames: Showdown.StatName[] = [
+  'hp',
+  'atk',
+  'def',
+  'spa',
+  'spd',
+  'spe',
+];
+
+/**
+ * Basically `PokemonStatNames` without `'hp'`,
+ * since you can't boost the Pokemon's HP... yet... (right, GameFreak...?)
+ */
+export const PokemonBoostNames: Showdown.StatNameNoHp[] = [
+  'atk',
+  'def',
+  'spa',
+  'spd',
+  'spe',
+];
