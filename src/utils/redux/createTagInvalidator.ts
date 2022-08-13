eb8707a2df2fca1d29415ac799c8426105cb3164
Keith Choison <keith.c@live.co.kr>
2022-08-12 23:02:15 -0700

    feat(redux): added redux utils

---
+++ src/utils/redux/createTagInvalidator.ts
@@ -0,0 +1,37 @@
+import type { ReduxTagType } from '@showdex/consts';
+import type { ReduxBasePayload } from './buildQueryUrl';
+import type { ReduxProvidedTag } from './createTagProvider';
+
+/* eslint-disable @typescript-eslint/indent */
+
+export const createTagInvalidator = <TPayload extends ReduxBasePayload>(
+  tagType: ReduxTagType,
+  additionalTags?: ReduxProvidedTag[] | ((result: TPayload) => ReduxProvidedTag[]),
+  omitAnyId?: boolean,
+) => (
+  result: TPayload,
+): ReduxProvidedTag[] => {
+  const tags = typeof additionalTags === 'function' ?
+    additionalTags(result) :
+    additionalTags;
+
+  if (!tagType) {
+    return Array.isArray(tags) ? tags.filter(Boolean) : [];
+  }
+
+  return [
+    ...(Array.isArray(tags) ? tags.filter(Boolean) : []),
+
+    !!result?.id && {
+      type: tagType,
+      id: result.id,
+    },
+
+    !omitAnyId && {
+      type: tagType,
+      id: '*',
+    },
+  ].filter(Boolean);
+};
+
+/* eslint-enable @typescript-eslint/indent */
---
+++ src/utils/redux/index.ts
@@ -0,0 +1,4 @@
+export * from './buildQueryUrl';
+export * from './createTagInvalidator';
+export * from './createTagProvider';
+export * from './transformPresetResponse';
