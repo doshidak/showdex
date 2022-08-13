7bae1289a736e7f0ff6128d5ba4c7a043f777504
Keith Choison <keith.c@live.co.kr>
2022-08-12 23:04:15 -0700

    feat(consts): added httpMethod and redux tagTypes

---
+++ src/consts/tagTypes.ts
@@ -0,0 +1,11 @@
+/**
+ * For use with RTK Query only.
+ *
+ * @since 0.1.3
+ */
+export type ReduxTagType =
+  | PokemonReduxTagType;
+
+export enum PokemonReduxTagType {
+  Preset = 'pokemon:preset',
+}
