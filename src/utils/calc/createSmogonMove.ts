aab7423733d296b5185c9abeec73459691ae98c4
Keith Choison <keith.c@live.co.kr>
2022-08-15 04:30:45 -0700

    refac(utils): replaced gen arg with dex in createSmogonMove

--- src/utils/calc/createSmogonMove.ts
+++ src/utils/calc/createSmogonMove.ts
@@ -1,22 +1,22 @@
 import { Move as SmogonMove } from '@smogon/calc';
-import type { GenerationNum, MoveName } from '@pkmn/data';
+import type { Generation, MoveName } from '@pkmn/data';
 import type { CalcdexPokemon } from '@showdex/redux/store';
 
 export const createSmogonMove = (
-  gen: GenerationNum,
+  dex: Generation,
   pokemon: CalcdexPokemon,
   moveName: MoveName,
 ): SmogonMove => {
-  if (!gen || !pokemon?.speciesForme || !moveName) {
+  if (!dex?.num || !pokemon?.speciesForme || !moveName) {
     return null;
   }
 
-  const smogonMove = new SmogonMove(gen, moveName, {
+  const smogonMove = new SmogonMove(dex, moveName, {
     species: pokemon?.rawSpeciesForme || pokemon?.speciesForme,
     ability: pokemon?.dirtyAbility ?? pokemon?.ability,
     item: pokemon?.dirtyItem ?? pokemon?.item,
-    useZ: gen === 7 && pokemon?.useUltimateMoves,
-    useMax: gen === 8 && pokemon?.useUltimateMoves,
+    useZ: dex.num === 7 && pokemon?.useUltimateMoves,
+    useMax: dex.num === 8 && pokemon?.useUltimateMoves,
     isCrit: pokemon?.criticalHit ?? false,
   });
 
