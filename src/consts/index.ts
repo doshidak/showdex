8fa0949f13f150d802103877a350ef8a67828cbf
Keith Choison <keith.c@live.co.kr>
2022-08-20 01:01:56 -0700

    feat(consts): added items consts

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -2,6 +2,7 @@ export * from './abilities';
 export * from './ansiColor';
 export * from './formats';
 export * from './httpMethod';
+export * from './items';
 export * from './natures';
 export * from './stats';
 export * from './tagTypes';
---
+++ src/consts/items.ts
@@ -0,0 +1,11 @@
+import type { ItemName } from '@pkmn/data';
+
+export const PokemonSpeedReductionItems: ItemName[] = [
+  <ItemName> 'Macho Brace',
+  <ItemName> 'Power Anklet',
+  <ItemName> 'Power Band',
+  <ItemName> 'Power Belt',
+  <ItemName> 'Power Bracer',
+  <ItemName> 'Power Lens',
+  <ItemName> 'Power Weight',
+];
