ba4b65c4730d5234037c0d1c15f6876b7bd16446
Keith Choison <keith.c@live.co.kr>
2022-08-20 23:20:58 -0700

    docs(utils): added some notes on calcPokemonHp

--- src/utils/calc/calcPokemonHp.ts
+++ src/utils/calc/calcPokemonHp.ts
@@ -1,5 +1,17 @@
 import type { CalcdexPokemon } from '@showdex/redux/store';
 
+/**
+ * Name is a bit misleading because this returns the **percentage** of the Pokemon's
+ * remaining HP, not the actual numerical value.
+ *
+ * * You'll need to multiply this percentage with the actual max HP value to estimate
+ *   the Pokemon's remaining HP value.
+ *   - Actual max HP value can be derived from the `maxhp` of a `ServerPokemon` or
+ *     calculating the HP stat value after EVs/IVs/nature are applied.
+ *
+ * @todo Rename this into what it actually does lol
+ * @since 0.1.0
+ */
 export const calcPokemonHp = (
   pokemon: DeepPartial<Showdown.Pokemon> | DeepPartial<CalcdexPokemon>,
 ): CalcdexPokemon['hp'] => (pokemon?.hp || 0) / (pokemon?.maxhp || 1);
