6cda7db3e45eeec740078b418af342f6812ccad5
Keith Choison <keith.c@live.co.kr>
2022-08-31 05:02:01 -0700

    feat(utils): added getResourceUrl util

--- src/utils/core/index.ts
+++ src/utils/core/index.ts
@@ -1,5 +1,6 @@
 export * from './clamp';
 export * from './getEnv';
 export * from './getExtensionId';
+export * from './getResourceUrl';
 export * from './runtimeFetch';
 export * from './upsizeArray';
