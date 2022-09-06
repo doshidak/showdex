2c69b776f0f3df74c7240ab19d6dbd8370bcd32d
Keith Choison <keith.c@live.co.kr>
2022-09-06 15:55:18 -0700

    feat: added BattleAbilities to global type dec

--- types/global.d.ts
+++ types/global.d.ts
@@ -8,5 +8,6 @@ declare module '*.module.scss' {
 
 declare const app: Showdown.ClientApp;
 declare const Dex: Showdown.Dex; /** @todo convert to declare class Dex in battle-dex.d.ts */
+declare const BattleAbilities: { [abilityid: string]: Showdown.Ability; };
 declare const BattleMovedex: { [moveid: string]: Showdown.Move; };
 declare const UserPopup: Showdown.UserPopup;
