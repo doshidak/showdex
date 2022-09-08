d1e23c400a88fbaef2b39c966c4222dbd9771767
Keith Choison <keith.c@live.co.kr>
2022-09-07 19:23:10 -0700

    feat(utils): added build-target env to printBuildInfo

--- src/utils/debug/printBuildInfo.ts
+++ src/utils/debug/printBuildInfo.ts
@@ -4,5 +4,6 @@ export const printBuildInfo = (): string => (
   env('package-name') +
   `-v${env('package-version')}` +
   `-b${env('package-build-date')}` +
+  `-${env('build-target', 'chrome')}` +
   `${__DEV__ ? '-dev' : ''}`
 );
