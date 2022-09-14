56029cd826d25e106edf9b97e0263338c6e373c5
Keith Choison <keith.c@live.co.kr>
2022-09-14 09:43:34 -0700

    refac(comps): removed unused ColorScheme context
    since it's now replaced by showdexSlice in Redux

--- src/components/app/ColorScheme/useColorScheme.ts
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import type { ColorScheme } from './ColorSchemeContext';
-import { ColorSchemeContext } from './ColorSchemeContext';
-
-export const useColorScheme = (): ColorScheme => React.useContext(ColorSchemeContext);
