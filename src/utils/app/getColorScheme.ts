f5f96d105a02e2dd8187b489c86e5d4e800958b9
Keith Choison <keith.c@live.co.kr>
2022-09-14 08:53:54 -0700

    refac(utils): separated getSystemColorScheme from getColorScheme

--- src/utils/app/getColorScheme.ts
+++ src/utils/app/getColorScheme.ts
@@ -1,9 +1,40 @@
-export type ColorScheme =
-  | 'light'
-  | 'dark';
+/**
+ * Determines the value of the `'system'` color scheme via `window.matchMedia()`.
+ *
+ * * Runs a media query checking if `prefers-color-scheme` is `dark`.
+ *   - Value is based on the user's OS-wide color scheme.
+ * * Returns `null` if the media query could not be executed.
+ *
+ * @since 1.0.2
+ */
+export const getSystemColorScheme = (): Showdown.ColorScheme => {
+  // just an old habit working with SSR; doesn't hurt to check tho!
+  if (typeof window === 'undefined') {
+    return null;
+  }
 
-export const getColorScheme = (): ColorScheme => {
-  const schemeFromPrefs: ColorScheme | 'system' = Dex?.prefs?.('theme');
+  const queryResult = window.matchMedia?.('(prefers-color-scheme: dark)');
+
+  // make sure we get a valid result back
+  if (typeof queryResult?.matches !== 'boolean') {
+    return null;
+  }
+
+  return queryResult.matches
+    ? 'dark'
+    : 'light';
+};
+
+/**
+ * Returns the color scheme from the client's options.
+ *
+ * * Defaults to the `'light'` color scheme in cases where the user's
+ *   color scheme preference could not be determined.
+ *
+ * @since 0.1.2
+ */
+export const getColorScheme = (): Showdown.ColorScheme => {
+  const schemeFromPrefs = Dex?.prefs?.('theme');
 
   switch (schemeFromPrefs) {
     case 'light':
@@ -12,18 +43,19 @@ export const getColorScheme = (): ColorScheme => {
     }
 
     case 'system': {
-      // just an old habit working with SSR; doesn't hurt to check tho!
-      if (typeof window === 'undefined') {
-        return 'light';
+      const systemScheme = getSystemColorScheme();
+
+      if (systemScheme) {
+        return systemScheme;
       }
 
-      const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-      return darkMode ? 'dark' : 'light';
+      break;
     }
 
     default: {
-      return 'light';
+      break;
     }
   }
+
+  return 'light';
 };
