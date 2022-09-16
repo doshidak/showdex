1136603bee784d5755a64ac973551268c49449c7
Keith Choison <keith.c@live.co.kr>
2022-09-15 20:30:24 -0700

    refac(types): moved BattleItems declaration to global

--- types/global.d.ts
+++ types/global.d.ts
@@ -10,6 +10,7 @@ declare const app: Showdown.ClientApp;
 declare const Config: Showdown.PSConfig;
 declare const Dex: Showdown.Dex; /** @todo convert to declare class Dex in battle-dex.d.ts */
 declare const BattleAbilities: Showdown.BattleAbilities;
+declare const BattleItems: Showdown.BattleItems;
 declare const BattleMovedex: Showdown.BattleMovedex;
 declare const BattleTeambuilderTable: Showdown.BattleTeambuilderTable;
 declare const UserPopup: Showdown.UserPopup;
