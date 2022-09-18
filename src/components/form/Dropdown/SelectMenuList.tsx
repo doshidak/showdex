b7d0a886a1c3d193dd254ec08e7e3451e30e7d7d
Keith Choison <keith.c@live.co.kr>
2022-09-18 05:30:30 -0700

    feat(comps): added Scrollable to dropdown SelectMenuList
    renders a custom scrollbar instead of the gross default system one,
    particularly on Windows.

--- src/components/form/Dropdown/SelectMenuList.tsx
+++ src/components/form/Dropdown/SelectMenuList.tsx
@@ -1,25 +1,44 @@
 import * as React from 'react';
 import cx from 'classnames';
+import { Scrollable } from '@showdex/components/ui';
 import type { GroupBase, MenuListProps } from 'react-select';
 import type { DropdownOption } from './Dropdown';
 import styles from './Dropdown.module.scss';
 
+export type SelectMenuListProps<
+  Option extends DropdownOption,
+  Multi extends boolean,
+  Group extends GroupBase<Option>,
+> = Modify<MenuListProps<Option, Multi, Group>, {
+  innerProps?: Omit<JSX.IntrinsicElements['div'], 'ref'>;
+}>;
+
 /* eslint-disable @typescript-eslint/indent */
 
+/**
+ * For `react-select` and `Scrollable` to work propertly, requires a patch for
+ * `MenuList` of `react-select` to accept a `innerScrollRef` prop, which will
+ * apply the focused option scrolling to the `scrollRef` of `Scrollable` since
+ * the `contentRef` itself (that the `innerRef` is applied to) does not scroll.
+ *
+ * @since 0.1.0
+ */
 export const SelectMenuList = <
   Option extends DropdownOption,
   Multi extends boolean,
   Group extends GroupBase<Option>,
 >({
+  innerScrollRef,
   innerRef,
   className,
   maxHeight,
   isMulti,
   innerProps,
   children,
-}: MenuListProps<Option, Multi, Group>): JSX.Element => (
-  <div
-    ref={innerRef}
+}: SelectMenuListProps<Option, Multi, Group>): JSX.Element => (
+  <Scrollable
+    scrollRef={innerScrollRef}
+    contentRef={innerRef}
     className={cx(
       styles.menuList,
       isMulti && styles.multi,
@@ -29,7 +48,7 @@ export const SelectMenuList = <
     {...innerProps}
   >
     {children}
-  </div>
+  </Scrollable>
 );
 
 /* eslint-enable @typescript-eslint/indent */
