19549bee195c799be8c3a477c01b72109b7bd7c3
Keith Choison <keith.c@live.co.kr>
2022-09-19 22:49:06 -0700

    feat(calcdex): implemented auth playerKey detection in Calcdex

--- src/pages/Calcdex/Calcdex.tsx
+++ src/pages/Calcdex/Calcdex.tsx
@@ -3,7 +3,10 @@ import cx from 'classnames';
 import { BuildInfo } from '@showdex/components/debug';
 import { Scrollable } from '@showdex/components/ui';
 import { useColorScheme } from '@showdex/redux/store';
-import { detectPlayerKeyFromBattle } from '@showdex/utils/battle';
+import {
+  detectAuthPlayerKeyFromBattle,
+  detectPlayerKeyFromBattle,
+} from '@showdex/utils/battle';
 // import { logger } from '@showdex/utils/debug';
 import { FieldCalc } from './FieldCalc';
 import { PlayerCalc } from './PlayerCalc';
@@ -12,14 +15,12 @@ import styles from './Calcdex.module.scss';
 
 interface CalcdexProps {
   battle?: Showdown.Battle;
-  // tooltips?: Showdown.BattleTooltips;
 }
 
 // const l = logger('@showdex/pages/Calcdex/Calcdex');
 
 export const Calcdex = ({
   battle,
-  // tooltips,
 }: CalcdexProps): JSX.Element => {
   const colorScheme = useColorScheme();
 
@@ -31,18 +32,7 @@ export const Calcdex = ({
     // setActiveIndex,
     setSelectionIndex,
     setAutoSelect,
-  } = useCalcdex({
-    battle,
-    // tooltips,
-  });
-
-  // l.debug(
-  //   'rendering...',
-  //   '\n', 'colorScheme', colorScheme,
-  //   '\n', 'p1.pokemon', battle?.p1?.pokemon,
-  //   '\n', 'p2.pokemon', battle?.p2?.pokemon,
-  //   '\n', 'state', state,
-  // );
+  } = useCalcdex({ battle });
 
   const {
     battleId,
@@ -56,28 +46,30 @@ export const Calcdex = ({
 
   // playerKey is a ref in case `battle` becomes `null`
   const playerKey = React.useRef(detectPlayerKeyFromBattle(battle));
-  const opponentKey = playerKey.current === 'p1' ? 'p2' : 'p1';
+  const opponentKey = playerKey.current === 'p2' ? 'p1' : 'p2';
+
+  // detect if the logged-in user is also a player (currently just for FieldCalc lol)
+  // (and is also a ref for the same reason as playerKey)
+  const authPlayerKey = React.useRef(detectAuthPlayerKeyFromBattle(battle));
 
   React.useEffect(() => {
-    if (playerKey.current) {
-      return;
+    const detectedKey = detectPlayerKeyFromBattle(battle);
+    const detectedAuthKey = detectAuthPlayerKeyFromBattle(battle);
+
+    if (detectedKey && playerKey.current !== detectedKey) {
+      playerKey.current = detectedKey;
     }
 
-    const detectedKey = detectPlayerKeyFromBattle(battle);
-
-    if (detectedKey) {
-      playerKey.current = detectedKey;
+    if (detectedAuthKey && authPlayerKey.current !== detectedAuthKey) {
+      authPlayerKey.current = detectedAuthKey;
     }
   }, [
     battle,
   ]);
 
+  // map the sides as the player and opponent to track them easier
   const player = playerKey.current === 'p1' ? p1 : p2;
-  const opponent = playerKey.current === 'p1' ? p2 : p1;
-
-  if (!state?.battleId) {
-    return null;
-  }
+  const opponent = opponentKey === 'p1' ? p1 : p2;
 
   return (
     <div
@@ -87,63 +79,67 @@ export const Calcdex = ({
         !!colorScheme && styles[colorScheme],
       )}
     >
-      <Scrollable className={styles.content}>
-        <BuildInfo
-          position="top-right"
-        />
+      {
+        !!battleId &&
+        <Scrollable className={styles.content}>
+          <BuildInfo
+            position="top-right"
+          />
 
-        <PlayerCalc
-          dex={dex}
-          gen={gen}
-          format={format}
-          rules={rules}
-          playerKey={playerKey.current}
-          player={player}
-          opponent={opponent}
-          field={field}
-          defaultName="Player"
-          onPokemonChange={updatePokemon}
-          onIndexSelect={(index) => setSelectionIndex(
-            playerKey.current,
-            index,
-          )}
-          onAutoSelectChange={(autoSelect) => setAutoSelect(
-            playerKey.current,
-            autoSelect,
-          )}
-        />
+          <PlayerCalc
+            dex={dex}
+            gen={gen}
+            format={format}
+            rules={rules}
+            playerKey={playerKey.current}
+            player={player}
+            opponent={opponent}
+            field={field}
+            defaultName="Player"
+            onPokemonChange={updatePokemon}
+            onIndexSelect={(index) => setSelectionIndex(
+              playerKey.current,
+              index,
+            )}
+            onAutoSelectChange={(autoSelect) => setAutoSelect(
+              playerKey.current,
+              autoSelect,
+            )}
+          />
 
-        <FieldCalc
-          className={styles.fieldCalc}
-          battleId={battleId}
-          gen={gen}
-          playerKey={playerKey.current}
-          field={field}
-          onFieldChange={updateField}
-        />
+          <FieldCalc
+            className={styles.fieldCalc}
+            battleId={battleId}
+            gen={gen}
+            authPlayerKey={authPlayerKey.current}
+            playerKey={playerKey.current}
+            field={field}
+            onFieldChange={updateField}
+          />
 
-        <PlayerCalc
-          className={styles.opponentCalc}
-          dex={dex}
-          gen={gen}
-          format={format}
-          rules={rules}
-          playerKey={opponentKey}
-          player={opponent}
-          opponent={player}
-          field={field}
-          defaultName="Opponent"
-          onPokemonChange={updatePokemon}
-          onIndexSelect={(index) => setSelectionIndex(
-            opponentKey,
-            index,
-          )}
-          onAutoSelectChange={(autoSelect) => setAutoSelect(
-            opponentKey,
-            autoSelect,
-          )}
-        />
-      </Scrollable>
+          <PlayerCalc
+            className={styles.opponentCalc}
+            dex={dex}
+            gen={gen}
+            format={format}
+            rules={rules}
+            playerKey={opponentKey}
+            player={opponent}
+            opponent={player}
+            field={field}
+            defaultName="Opponent"
+            onPokemonChange={updatePokemon}
+            onIndexSelect={(index) => setSelectionIndex(
+              opponentKey,
+              index,
+            )}
+            onAutoSelectChange={(autoSelect) => setAutoSelect(
+              opponentKey,
+              autoSelect,
+            )}
+          />
+        </Scrollable>
+      }
     </div>
   );
 };
