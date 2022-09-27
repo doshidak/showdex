dfed44514546132a834364176fdde74705e7a770
Keith Choison <keith.c@live.co.kr>
2022-09-26 17:38:22 -0700

    refac(utils): added moves const to get max/z move utils

--- src/utils/battle/getZMove.ts
+++ src/utils/battle/getZMove.ts
@@ -1,27 +1,7 @@
+import { PokemonSpecialZMoves, PokemonZMoves } from '@showdex/consts';
+import { formatId } from '@showdex/utils/app';
 import { logger } from '@showdex/utils/debug';
 import type { ItemName, MoveName } from '@pkmn/data';
-
-const PokemonZMoveTypings: Record<Showdown.TypeName, MoveName> = {
-  '???': null,
-  Poison: <MoveName> 'Acid Downpour',
-  Fighting: <MoveName> 'All-Out Pummeling',
-  Dark: <MoveName> 'Black Hole Eclipse',
-  Grass: <MoveName> 'Bloom Doom',
-  Normal: <MoveName> 'Breakneck Blitz',
-  Rock: <MoveName> 'Continental Crush',
-  Steel: <MoveName> 'Corkscrew Crash',
-  Dragon: <MoveName> 'Devastating Drake',
-  Electric: <MoveName> 'Gigavolt Havoc',
-  Water: <MoveName> 'Hydro Vortex',
-  Fire: <MoveName> 'Inferno Overdrive',
-  Ghost: <MoveName> 'Never-Ending Nightmare',
-  Bug: <MoveName> 'Savage Spin-Out',
-  Psychic: <MoveName> 'Shattered Psyche',
-  Ice: <MoveName> 'Subzero Slammer',
-  Flying: <MoveName> 'Supersonic Skystrike',
-  Ground: <MoveName> 'Tectonic Rage',
-  Fairy: <MoveName> 'Twinkle Tackle',
-};
 
 const l = logger('@showdex/utils/app/getZMove');
 
@@ -54,6 +34,7 @@ export const getZMove = (
         // '\n', 'typeof dex.moves.get', typeof dex?.moves?.get,
         '\n', 'moveName', moveName,
         '\n', 'itemName', itemName,
+        '\n', 'itemOnly?', itemOnly,
         '\n', '(You will only see this warning on development.)',
       );
     }
@@ -70,6 +51,7 @@ export const getZMove = (
         '\n', 'move', move,
         '\n', 'moveName', moveName,
         '\n', 'itemName', itemName,
+        '\n', 'itemOnly?', itemOnly,
         '\n', '(You will only see this warning on development.)',
       );
     }
@@ -84,147 +66,23 @@ export const getZMove = (
     return null;
   }
 
-  if (move.name.includes('Hidden Power')) {
-    return PokemonZMoveTypings.Normal;
+  const moveId = formatId(move.name);
+
+  // note: for Z moves, Hidden Power will ALWAYS be treated as a Normal type
+  if (moveId.includes('hiddenpower')) {
+    return PokemonZMoves.Normal;
   }
 
   const item = itemName ? Dex.items.get(itemName) : null;
+  const itemId = item?.exists && item.name ? formatId(item.name) : null;
 
   // if (!item?.megaEvolves) {
   //   return null;
   // }
 
   // check for speical Z moves
-  if (item?.exists) {
-    switch (move.name) {
-      case 'Clanging Scales': {
-        if (item.name === 'Kommonium Z') {
-          return <MoveName> 'Clangorous Soulblaze';
-        }
-
-        break;
-      }
-
-      case 'Darkest Lariat': {
-        if (item.name === 'Incinium Z') {
-          return <MoveName> 'Malicious Moonsault';
-        }
-
-        break;
-      }
-
-      case 'Giga Impact': {
-        if (item.name === 'Snorlium Z') {
-          return <MoveName> 'Pulverizing Pancake';
-        }
-
-        break;
-      }
-
-      case 'Moongeist Beam': {
-        if (item.name === 'Lunalium Z') {
-          return <MoveName> 'Menacing Moonraze Maelstrom';
-        }
-
-        break;
-      }
-
-      case 'Nature\'s Madness': {
-        if (item.name === 'Tapunium Z') {
-          return <MoveName> 'Guardian of Alola';
-        }
-
-        break;
-      }
-
-      case 'Photon Geyser': {
-        if (item.name === 'Ultranecrozium Z') {
-          return <MoveName> 'Light That Burns the Sky';
-        }
-
-        break;
-      }
-
-      case 'Play Rough': {
-        if (item.name === 'Mimikium Z') {
-          return <MoveName> 'Let\'s Snuggle Forever';
-        }
-
-        break;
-      }
-
-      case 'Psychic': {
-        if (item.name === 'Mewnium Z') {
-          return <MoveName> 'Genesis Supernova';
-        }
-
-        break;
-      }
-
-      case 'Sparkling Aria': {
-        if (item.name === 'Primarium Z') {
-          return <MoveName> 'Oceanic Operetta';
-        }
-
-        break;
-      }
-
-      case 'Spectral Thief': {
-        if (item.name === 'Marshadium Z') {
-          return <MoveName> 'Soul-Stealing 7-Star Strike';
-        }
-
-        break;
-      }
-
-      case 'Spirit Shackle': {
-        if (item.name === 'Decidium Z') {
-          return <MoveName> 'Sinister Arrow Raid';
-        }
-
-        break;
-      }
-
-      case 'Stone Edge': {
-        if (item.name === 'Lycanium Z') {
-          return <MoveName> 'Splintered Stormshards';
-        }
-
-        break;
-      }
-
-      case 'Sunsteel Strike': {
-        if (item.name === 'Solganium Z') {
-          return <MoveName> 'Searing Sunraze Smash';
-        }
-
-        break;
-      }
-
-      case 'Thunderbolt': {
-        if (item.name === 'Pikashunium Z') {
-          return <MoveName> '10,000,000 Volt Thunderbolt';
-        }
-
-        if (item.name === 'Aloraichium Z') {
-          return <MoveName> 'Stoked Sparksurfer';
-        }
-
-        break;
-      }
-
-      case 'Volt Tackle': {
-        if (item.name === 'Pikanium Z') {
-          return <MoveName> 'Catastropika';
-        }
-
-        break;
-      }
-
-      default: {
-        break;
-      }
-    }
+  if (itemId && PokemonSpecialZMoves[moveId] && PokemonSpecialZMoves[moveId][itemId]) {
+    return PokemonSpecialZMoves[moveId][itemId];
   }
 
   // if an itemName was provided and the item's Z typing doesn't match the move's typing,
@@ -233,5 +91,5 @@ export const getZMove = (
     return null;
   }
 
-  return PokemonZMoveTypings[move.type];
+  return PokemonZMoves[move.type];
 };
