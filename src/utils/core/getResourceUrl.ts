7dbe68e7813d10b41e5d5ea28bc7f68cba0b0c01
Keith Choison <keith.c@live.co.kr>
2022-09-28 00:53:56 -0700

    refac(utils): added getExtensionProtocol to getResourceUrl

--- src/utils/core/getResourceUrl.ts
+++ src/utils/core/getResourceUrl.ts
@@ -1,16 +1,13 @@
-import { env } from './getEnv';
 import { getExtensionId } from './getExtensionId';
+import { getExtensionProtocol } from './getExtensionProtocol';
 
 export const getResourceUrl = (fileName: string): string => {
+  const protocol = getExtensionProtocol();
   const extensionId = getExtensionId();
 
   if (!extensionId || !fileName) {
     return null;
   }
-
-  const protocol = env('build-target') === 'firefox'
-    ? 'moz-extension'
-    : 'chrome-extension';
 
   return `${protocol}://${extensionId}/${fileName}`;
 };
