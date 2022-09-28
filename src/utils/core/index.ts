92a0c34165040cec9372ec0956947e363e8efd37
Keith Choison <keith.c@live.co.kr>
2022-09-28 00:53:26 -0700

    feat(utils): added getExtensionProtocol

--- src/utils/core/index.ts
+++ src/utils/core/index.ts
@@ -2,6 +2,7 @@ export * from './capitalize';
 export * from './clamp';
 export * from './getEnv';
 export * from './getExtensionId';
+export * from './getExtensionProtocol';
 export * from './getResourceUrl';
 export * from './runtimeFetch';
 export * from './upsizeArray';
