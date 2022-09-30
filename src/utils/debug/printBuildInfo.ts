beb13e9844cfc79f897deec4dfc8164a9b1bf9e4
Keith Choison <keith.c@live.co.kr>
2022-09-29 21:40:16 -0700

    feat(utils): updated printBuildInfo to match output file name

--- src/utils/debug/printBuildInfo.ts
+++ src/utils/debug/printBuildInfo.ts
@@ -1,9 +1,9 @@
 import { env } from '@showdex/utils/core';
 
 export const printBuildInfo = (): string => (
-  env('package-name') +
-  `-v${env('package-version')}` +
-  `-b${env('build-date')}` +
-  `-${env('build-target', 'chrome')}` +
-  `${__DEV__ ? '-dev' : ''}`
+  env('package-name')
+    + `-v${env('package-version')}`
+    + `-b${env('build-date')}`
+    + `${__DEV__ ? '-dev' : ''}`
+    + `.${env('build-target', 'chrome')}`
 );
