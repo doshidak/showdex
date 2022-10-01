ad3675a39f24757bcf33a668c417605b7cedf0aa
Keith Choison <keith.c@live.co.kr>
2022-09-30 21:20:48 -0700

    feat(utils): added alwaysCriticalHits in createSmogonMove

--- src/utils/calc/createSmogonMove.ts
+++ src/utils/calc/createSmogonMove.ts
@@ -1,28 +1,40 @@
 import { Move as SmogonMove } from '@smogon/calc';
 // import { formatId } from '@showdex/utils/app';
-import { getDexForFormat } from '@showdex/utils/battle';
-import type { Generation, MoveName } from '@smogon/calc/dist/data/interface';
+import { getGenDexForFormat, getMaxMove, getZMove } from '@showdex/utils/battle';
+import type { MoveName } from '@smogon/calc/dist/data/interface';
 import type { CalcdexPokemon } from '@showdex/redux/store';
+import { alwaysCriticalHits } from './alwaysCriticalHits';
 
 export const createSmogonMove = (
-  // gen: GenerationNum,
   format: string,
   pokemon: CalcdexPokemon,
   moveName: MoveName,
 ): SmogonMove => {
   // using the Dex global for the gen arg of SmogonMove seems to work here lol
-  const dex = <Generation> <unknown> getDexForFormat(format);
+  const dex = getGenDexForFormat(format);
 
   if (!dex || !format || !pokemon?.speciesForme || !moveName) {
     return null;
   }
 
+  const ability = pokemon.dirtyAbility ?? pokemon.ability;
+  const item = pokemon.dirtyItem ?? pokemon.item;
+
   return new SmogonMove(dex, moveName, {
     species: pokemon.speciesForme,
-    ability: pokemon.dirtyAbility ?? pokemon.ability,
-    item: pokemon.dirtyItem ?? pokemon.item,
-    useZ: pokemon.useZ && !pokemon.useMax, // only apply one of them, not both!
+
+    ability,
+    item,
+
+    // only apply one of them, not both!
+    useZ: pokemon.useZ && !pokemon.useMax,
     useMax: pokemon.useMax,
-    isCrit: pokemon.criticalHit,
+
+    // for moves that always crit, we need to make sure the crit doesn't apply when Z/Max'd
+    isCrit: (
+      alwaysCriticalHits(moveName, format)
+        && (!pokemon.useZ || !getZMove(moveName, item))
+        && (!pokemon.useMax || !getMaxMove(moveName, ability, pokemon.speciesForme))
+    ) || pokemon.criticalHit,
   });
 };
