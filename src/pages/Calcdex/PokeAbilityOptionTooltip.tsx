fc04561ff16a961362011c8bfafdb8a74067793e
Keith Choison <keith.c@live.co.kr>
2022-10-10 18:45:02 -0700

    feat(calcdex): added optionTooltip components

---
+++ src/pages/Calcdex/PokeAbilityOptionTooltip.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import cx from 'classnames';
+import { formatDexDescription, getDexForFormat } from '@showdex/utils/battle';
+import type { AbilityName } from '@smogon/calc/dist/data/interface';
+import type { SelectOptionTooltipProps } from '@showdex/components/form';
+import styles from './PokeInfo.module.scss';
+
+export interface PokeAbilityOptionTooltipProps extends SelectOptionTooltipProps<AbilityName> {
+  className?: string;
+  style?: React.CSSProperties;
+  format?: string;
+}
+
+export const PokeAbilityOptionTooltip = ({
+  className,
+  style,
+  format,
+  value,
+  hidden,
+}: PokeAbilityOptionTooltipProps): JSX.Element => {
+  if (!value || hidden) {
+    return null;
+  }
+
+  const dex = getDexForFormat(format);
+  const dexAbility = dex?.abilities.get(value);
+  const description = formatDexDescription(dexAbility?.shortDesc || dexAbility?.desc);
+
+  if (!description) {
+    return null;
+  }
+
+  return (
+    <div
+      className={cx(
+        styles.tooltipContent,
+        styles.descTooltip,
+        className,
+      )}
+      style={style}
+    >
+      {description}
+    </div>
+  );
+};
