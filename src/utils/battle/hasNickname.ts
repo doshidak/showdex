e467a1bd293fdc4b8f86bfdfcb3f7f172bbc62f4
Keith Choison <keith.c@live.co.kr>
2022-10-11 03:20:51 -0700

    feat(utils): simplified hasNickname detection logic

--- src/utils/battle/hasNickname.ts
+++ src/utils/battle/hasNickname.ts
@@ -1,6 +1,6 @@
-import { formatId } from '@showdex/utils/app';
+// import { formatId } from '@showdex/utils/app';
 import type { CalcdexPokemon } from '@showdex/redux/store';
-import { getDexForFormat } from './getDexForFormat';
+// import { getDexForFormat } from './getDexForFormat';
 
 /**
  * Whether the passed-in `pokemon` has a nickname.
@@ -14,14 +14,17 @@ export const hasNickname = (
     return false;
   }
 
-  const dex = getDexForFormat();
-  const dexSpecies = dex?.species.get(pokemon.speciesForme);
+  // const dex = getDexForFormat();
+  // const dexSpecies = dex?.species.get(pokemon.speciesForme);
 
-  return ![
-    pokemon.speciesForme,
-    dexSpecies?.baseSpecies,
-    dexSpecies?.baseForme,
-    ...(dexSpecies?.otherFormes || []),
-    ...(dexSpecies?.cosmeticFormes || []),
-  ].filter(Boolean).map((s) => formatId(s)).includes(formatId(pokemon.name));
+  // return ![
+  //   pokemon.speciesForme,
+  //   dexSpecies?.baseSpecies,
+  //   dexSpecies?.baseForme,
+  //   ...(dexSpecies?.otherFormes || []),
+  //   ...(dexSpecies?.cosmeticFormes || []),
+  // ].filter(Boolean).map((s) => formatId(s)).includes(formatId(pokemon.name));
+
+  return !pokemon.speciesForme.endsWith('-*')
+    && pokemon.name !== pokemon.speciesForme;
 };
