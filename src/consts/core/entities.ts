e738583badbf51efb2881b275073b2b3e3c50518
Keith Choison <keith.c@live.co.kr>
2022-10-15 23:13:42 -0700

    feat(consts): added additional html entities

--- src/consts/core/entities.ts
+++ src/consts/core/entities.ts
@@ -1,6 +1,7 @@
 /**
  * HTML `&times;` entity, represented as `U+00D7`.
  *
+ * @example '×'
  * @since 1.0.3
  */
 export const times = '\u00D7';
@@ -8,6 +9,55 @@ export const times = '\u00D7';
 /**
  * HTML `&eacute;` entity, represented as `U+00E9`.
  *
+ * @example 'é'
  * @since 1.0.3
  */
 export const eacute = '\u00E9';
+
+/**
+ * Bullet operator (not an HTML entity), represented as `U+2219`.
+ *
+ * @example '∙'
+ * @since 1.0.4
+ */
+export const bullop = '\u2219';
+
+/**
+ * HTML `&bull;` entity, represented as `U+2022`.
+ *
+ * @example '•'
+ * @since 1.0.4
+ */
+export const bull = '\u2022';
+
+/**
+ * HTML `&lsaquo;` entity, represented as `U+2039`.
+ *
+ * @example '‹'
+ * @since 1.0.4
+ */
+export const lsaquo = '\u2039';
+
+/**
+ * HTML `&rsaquo;` entity, represented as `U+203A`.
+ *
+ * @example '›'
+ * @since 1.0.4
+ */
+export const rsaquo = '\u203A';
+
+/**
+ * HTML `&laquo;` entity, represented as `U+00AB`.
+ *
+ * @example '«'
+ * @since 1.0.4
+ */
+export const laquo = '\u00AB';
+
+/**
+ * HTML `&raquo;` entity, represented as `U+00BB`.
+ *
+ * @example '»'
+ * @since 1.0.4
+ */
+export const raquo = '\u00BB';
