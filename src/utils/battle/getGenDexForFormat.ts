f8d2e7cf260056375bb48b0c43a8a592f69a46eb
Keith Choison <keith.c@live.co.kr>
2022-10-23 19:43:35 -0700

    fix(utils): fixed non-applying Eviolite in getGenDexForFormat

--- src/utils/battle/getGenDexForFormat.ts
+++ src/utils/battle/getGenDexForFormat.ts
@@ -1,10 +1,11 @@
 import { env } from '@showdex/utils/core';
 import type { GenerationNum } from '@smogon/calc';
-import type { Generation } from '@smogon/calc/dist/data/interface';
+import type { Generation, Specie } from '@smogon/calc/dist/data/interface';
 import { detectGenFromFormat } from './detectGenFromFormat';
 import { getDexForFormat } from './getDexForFormat';
 import { getNaturesDex } from './getNaturesDex';
 import { getTypesDex } from './getTypesDex';
+import { notFullyEvolved } from './notFullyEvolved';
 
 /**
  * Returns a somewhat compatible `Generation` dex (same one from `@pkmn/data`) based on the
@@ -32,10 +33,28 @@ export const getGenDexForFormat = (format: string | GenerationNum): Generation =
         : env.int<GenerationNum>('calcdex-default-gen')
     );
 
+  // we need to override dex.species.get() to populate the `nfe` property
+  // since it's not provided by Showdown's global Dex
+  // (otherwise, Eviolite won't work since the mechanics file calls this again!)
+  const species: Generation['species'] = {
+    ...(<Generation['species']> <unknown> dex.species),
+
+    get: (id) => {
+      const specie = <Specie> <unknown> dex.species.get(id);
+
+      if (typeof specie?.nfe !== 'boolean') {
+        (<Writable<Specie>> specie).nfe = notFullyEvolved(id);
+      }
+
+      return specie;
+    },
+  };
+
   return <Generation> <unknown> {
     ...dex,
     num: gen,
     natures: getNaturesDex(),
+    species,
     types: getTypesDex(gen),
   };
 };
