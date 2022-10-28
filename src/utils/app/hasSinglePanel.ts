40660b87b50d5a8ffe4fe82fff101722df5ce77b
Keith Choison <keith.c@live.co.kr>
2022-10-28 04:04:01 -0700

    feat(utils): added BattleRoom breakpoint in hasSinglePanel

--- src/utils/app/hasSinglePanel.ts
+++ src/utils/app/hasSinglePanel.ts
@@ -3,8 +3,15 @@
  *
  * * Will return `false` if the layout has left-right panels or the client's options
  *   couldn't be determined.
+ * * Appears that while in battle, a viewport width less than `1275px` will collapse
+ *   into a single panel.
  *
  * @default false
  * @since 1.0.3
  */
-export const hasSinglePanel = (): boolean => Dex?.prefs?.('onepanel') || false;
+export const hasSinglePanel = (): boolean => (
+  app.curRoom?.id?.startsWith('battle-')
+    && $?.(window).width() < 1275
+)
+  || Dex?.prefs?.('onepanel')
+  || false;
