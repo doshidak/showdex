963e036832c33d2ab40ac6b1287c8881d12204aa
Keith Choison <keith.c@live.co.kr>
2022-11-09 16:34:15 -0800

    fix(utils): fixed missing learnsets in getPokemonLearnset
    forgot to consider battleOnly, changesFrom, and prevo properties from
    the species dex

--- src/utils/battle/getPokemonLearnset.ts
+++ src/utils/battle/getPokemonLearnset.ts
@@ -72,7 +72,7 @@ export const getPokemonLearnset = (
 
   // e.g., 'Urshifu-*' -> 'urshifu', 'Urshifu-Rapid-Strike' -> 'urshifurapidstrike',
   // 'Urshifu-Rapid-Strike-Gmax' -> 'urshifurapidstrike', 'Charizard-Mega-X' -> 'charizard'
-  const speciesId = formatId(speciesForme.replace(/(?:-\*|-Mega(?:-[A-Z]+)?|-Gmax)$/i, ''));
+  // const speciesId = formatId(speciesForme.replace(/(?:-\*|-Mega(?:-[A-Z]+)?|-Gmax)$/i, ''));
 
   // note: not all formats (like metronome) include learnsets, hence the conditional spreading below
   const formatKey = guessTableFormatKey(format);
@@ -82,15 +82,47 @@ export const getPokemonLearnset = (
     ...(!!formatKey && formatKey in BattleTeambuilderTable && BattleTeambuilderTable[formatKey]?.learnsets),
   };
 
-  const learnsetKey = speciesId in learnsets
-    ? speciesId
-    : Object.keys(learnsets).find((k) => speciesId.includes(k));
+  // find all the species (including previous evolutions) to lookup learnsets for
+  // (e.g., Weavile's prevo is Sneasel, which includes moves that don't exist in Weavile's learnset like Icicle Crash)
+  const speciesIdLookups: string[] = [];
+  let currentDexSpecies = dex.species.get(speciesForme);
 
-  if (!learnsetKey) {
+  while (currentDexSpecies?.exists) {
+    const {
+      id,
+      baseSpecies,
+      battleOnly: battleOnlyFromDex,
+      changesFrom,
+      prevo,
+    } = currentDexSpecies;
+
+    // battleOnly could be 'Charizard' (from 'Charizard-Mega-Y', for instance) or
+    // ['Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane'] (from 'Necrozma-Ultra'), which we'll ignore if that's the case
+    const battleOnly = typeof battleOnlyFromDex === 'string'
+      ? battleOnlyFromDex
+      : null;
+
+    // only push to speciesIdLookups if the formatted id exists in learnsets
+    if (id in learnsets) {
+      speciesIdLookups.push(id);
+    } else if (battleOnly && battleOnly !== baseSpecies && formatId(battleOnly) in learnsets) {
+      speciesIdLookups.push(formatId(battleOnly));
+    } else if (formatId(baseSpecies) in learnsets) {
+      speciesIdLookups.push(formatId(baseSpecies));
+    }
+
+    const nextSpecies = battleOnly || changesFrom || prevo;
+
+    currentDexSpecies = nextSpecies
+      ? dex.species.get(nextSpecies)
+      : null;
+  }
+
+  if (!speciesIdLookups.length) {
     if (__DEV__) {
       l.warn(
-        'Failed to obtain a valid learnsetKey for speciesForme', speciesForme,
-        '\n', 'speciesId', speciesId, 'learnsetKey', learnsetKey,
+        'Failed to obtain any speciesIdLookups for speciesForme', speciesForme,
+        '\n', 'speciesIdLookups', speciesIdLookups,
         '\n', 'format', format, 'gen', gen,
         '\n', '(You will only see this warning on development.)',
       );
@@ -99,11 +131,25 @@ export const getPokemonLearnset = (
     return [];
   }
 
-  // e.g., { attract: '45678pqg', auroraveil: '8g', ... }
-  const learnset = Object.entries(learnsets[learnsetKey])
-    .filter(([, gens]) => ignoreGen || gens.includes(String(gen)))
-    .map(([id]) => <MoveName> dex.moves.get(id)?.name)
-    .filter(Boolean);
+  const learnset: MoveName[] = Array.from(
+    new Set(
+      speciesIdLookups.flatMap((speciesId) => {
+        const learnsetKey = speciesId in learnsets
+          ? speciesId // should always be this case, but checking again just in case
+          : Object.keys(learnsets).find((k) => speciesId.includes(k));
+
+        if (!learnsetKey) {
+          return [];
+        }
+
+        // e.g., { attract: '45678pqg', auroraveil: '8g', ... }
+        return Object.entries(learnsets[learnsetKey])
+          .filter(([, gens]) => ignoreGen || gens.includes(String(gen)))
+          .map(([id]) => <MoveName> dex.moves.get(id)?.name)
+          .filter(Boolean);
+      }),
+    ),
+  ).sort();
 
   return learnset;
 };
