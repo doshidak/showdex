05d42d7b62a60b9ae9a9e8f6ceaebf8bf34ce98f
Keith Choison <keith.c@live.co.kr>
2022-11-12 19:44:42 -0800

    feat(comps): added PokeFormeTooltip

--- src/components/app/index.ts
+++ src/components/app/index.ts
@@ -1,5 +1,6 @@
 export * from './Picon';
 export * from './PiconButton';
+export * from './PokeFormeTooltip';
 export * from './PokeHpBar';
 export * from './PokeStatus';
 export * from './PokeType';
