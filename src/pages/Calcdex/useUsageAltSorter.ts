a4eb81618bf55300809cb0216636ed432c99ec2a
Keith Choison <keith.c@live.co.kr>
2022-11-16 23:17:08 -0800

    feat(calcdex): added useUsageAltSorter hook

---
+++ src/pages/Calcdex/useUsageAltSorter.ts
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { usageAltPercentFinder, usageAltPercentSorter } from '@showdex/utils/battle';
+import type { CalcdexPokemonAlt } from '@showdex/redux/store';
+import type { CalcdexPokemonUsageAltSorter } from '@showdex/utils/battle';
+
+/* eslint-disable @typescript-eslint/indent */
+
+/**
+ * Hook that memoizes the output function of the `usageAltPercentSorter()` factory.
+ *
+ * * Currently only used internally by `Calcdex` children components.
+ *   - Namely in `PokeInfo`.
+ *
+ * @since 1.0.7
+ */
+export const useUsageAltSorter = <
+  T extends string,
+>(
+  alts: CalcdexPokemonAlt<T>[],
+): CalcdexPokemonUsageAltSorter<T> => {
+  const findUsagePercent = React.useMemo(() => (
+    Array.isArray(alts)
+      ? usageAltPercentFinder(alts)
+      : null
+  ), [
+    alts,
+  ]);
+
+  const usageSorter = React.useMemo(() => (
+    typeof findUsagePercent === 'function'
+      ? usageAltPercentSorter(findUsagePercent)
+      : null
+  ), [
+    findUsagePercent,
+  ]);
+
+  return usageSorter;
+};
+
+/* eslint-enable @typescript-eslint/indent */
