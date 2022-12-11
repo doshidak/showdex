2d76e65503e10258bcc62a065370f5e0021784ad
Keith Choison <keith.c@live.co.kr>
2022-12-10 20:14:10 -0800

    feat(utils): added shouldIgnoreItem

---
+++ src/utils/battle/shouldIgnoreItem.ts
@@ -0,0 +1,28 @@
+import { PokemonSpeedReductionItems } from '@showdex/consts/pokemon';
+import { formatId } from '@showdex/utils/app';
+import type { CalcdexBattleField, CalcdexPokemon } from '@showdex/redux/store';
+import { hasMegaForme } from './hasMegaForme';
+
+/**
+ * Determines whether the `pokemon`'s item's effects should be ignored.
+ *
+ * @since 1.1.0
+ */
+export const shouldIgnoreItem = (
+  pokemon: CalcdexPokemon,
+  field?: CalcdexBattleField,
+): boolean => {
+  const speciesForme = pokemon?.transformedForme || pokemon?.speciesForme;
+
+  if (!speciesForme) {
+    return false;
+  }
+
+  const ability = formatId(pokemon.dirtyAbility || pokemon.ability);
+  const item = formatId(pokemon.dirtyItem ?? pokemon.item);
+
+  return 'embargo' in pokemon.volatiles // Embargo is a move
+    || hasMegaForme(speciesForme)
+    || field?.isMagicRoom
+    || (ability === 'klutz' && !PokemonSpeedReductionItems.map((i) => formatId(i).includes(item)));
+};
