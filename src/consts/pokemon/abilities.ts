e178fdb2accd04f13922ed3e4e587d589c8b29fa
Keith Choison <keith.c@live.co.kr>
2022-12-10 20:22:21 -0800

    feat(consts): added Ruin abilities to toggleable list

--- src/consts/pokemon/abilities.ts
+++ src/consts/pokemon/abilities.ts
@@ -9,13 +9,19 @@ import type { AbilityName } from '@smogon/calc/dist/data/interface';
  * @since 0.1.2
  */
 export const PokemonToggleAbilities: AbilityName[] = [
+  <AbilityName> 'Beads of Ruin',
   <AbilityName> 'Flash Fire',
   // <AbilityName> 'Intimidate', // applies the ATK reduction within `boosts`, so no need to "toggle" this
   <AbilityName> 'Minus',
   <AbilityName> 'Multiscale', // special case based off the HP, but specified here to allow toggling in the UI
   <AbilityName> 'Plus',
+  <AbilityName> 'Protosynthesis', // introduced gen 9
+  <AbilityName> 'Quark Drive', // introduced gen 9
   <AbilityName> 'Shadow Shield', // basically another multiscale lmao
   <AbilityName> 'Slow Start',
   <AbilityName> 'Stakeout',
+  <AbilityName> 'Sword of Ruin',
+  <AbilityName> 'Tablets of Ruin',
   <AbilityName> 'Unburden',
+  <AbilityName> 'Vessel of Ruin',
 ];
