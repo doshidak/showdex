ee3ce31c1b9fd8bcf8589b80e0769143a4f03bf5
Keith Choison <keith.c@live.co.kr>
2022-12-13 02:29:15 -0800

    refac(utils): refactored moveOverrides utils

--- src/utils/calc/calcSmogonMatchup.ts
+++ src/utils/calc/calcSmogonMatchup.ts
@@ -1,5 +1,5 @@
 import { calculate } from '@smogon/calc';
-import { formatId } from '@showdex/utils/app';
+// import { formatId } from '@showdex/utils/app';
 import { getGenDexForFormat } from '@showdex/utils/battle';
 import { logger } from '@showdex/utils/debug';
 import type { Move as SmogonMove, Pokemon as SmogonPokemon } from '@smogon/calc';
@@ -135,29 +135,29 @@ export const calcSmogonMatchup = (
     return matchup;
   }
 
-  const ability = formatId(playerPokemon.dirtyAbility || playerPokemon.ability);
+  // const ability = formatId(playerPokemon.dirtyAbility || playerPokemon.ability);
 
   // apply base power mods
-  const basePowerMods: number[] = [];
+  // const basePowerMods: number[] = [];
 
-  const playerSideKey: keyof CalcdexBattleField = playerKey === 'p2' ? 'defenderSide' : 'attackerSide';
-  const playerSide = field?.[playerSideKey];
+  // const playerSideKey: keyof CalcdexBattleField = playerKey === 'p2' ? 'defenderSide' : 'attackerSide';
+  // const playerSide = field?.[playerSideKey];
 
   /**
    * @todo This implementation does not allow us to display the increased base power in the UI,
    *   like how it's for *Rage Fist*.
    */
-  if (ability === 'supremeoverlord' && playerSide?.faintedCount > 0) {
-    /** @todo replace `5` with `maxPokemon - 1` from `CalcdexPlayer` whenever you refactor the codebase lmao */
-    basePowerMods.push(1 + (0.1 * Math.min(playerSide.faintedCount, 5)));
-  }
+  // if (ability === 'supremeoverlord' && playerSide?.faintedCount > 0) {
+  //   /** @todo replace `5` with `maxPokemon - 1` from `CalcdexPlayer` whenever you refactor the codebase lmao */
+  //   basePowerMods.push(1 + (0.1 * Math.min(playerSide.faintedCount, 5)));
+  // }
 
-  if (formatId(opponentPokemon.lastMove) === 'glaiverush') {
-    basePowerMods.push(2);
-  }
+  // if (formatId(opponentPokemon.lastMove) === 'glaiverush') {
+  //   basePowerMods.push(2);
+  // }
 
   matchup.attacker = createSmogonPokemon(format, playerPokemon, field);
-  matchup.move = createSmogonMove(format, playerPokemon, playerMove, basePowerMods);
+  matchup.move = createSmogonMove(format, playerPokemon, playerMove, opponentPokemon);
   matchup.defender = createSmogonPokemon(format, opponentPokemon, field);
 
   const smogonField = createSmogonField(field, playerPokemon, playerKey);
