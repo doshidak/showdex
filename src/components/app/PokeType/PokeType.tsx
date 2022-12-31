c686ee3cff478aeff9bce86b56a2c1e7feff0eb9
Keith Choison <keith.c@live.co.kr>
2022-12-30 20:45:37 -0800

    feat(comps): added containerSize to PokeType

--- src/components/app/PokeType/PokeType.tsx
+++ src/components/app/PokeType/PokeType.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import cx from 'classnames';
-import { PokemonTypeShortAbbreviations, PokemonTypeAbbreviations } from '@showdex/consts/pokemon';
+import { PokemonTypeLabels } from '@showdex/consts/pokemon';
 import { useColorScheme } from '@showdex/redux/store';
+import type { ElementSizeLabel } from '@showdex/utils/hooks';
 import styles from './PokeType.module.scss';
 
 export interface PokeTypeProps {
@@ -10,7 +11,7 @@ export interface PokeTypeProps {
   type?: Showdown.TypeName;
   defaultLabel?: string;
   reverseColorScheme?: boolean;
-  shorterAbbreviations?: boolean;
+  containerSize?: ElementSizeLabel;
   highlight?: boolean;
 }
 
@@ -20,18 +21,33 @@ export const PokeType = ({
   type,
   defaultLabel,
   reverseColorScheme,
-  shorterAbbreviations,
+  containerSize,
   highlight = true,
 }: PokeTypeProps): JSX.Element => {
   const currentColorScheme = useColorScheme();
+
   const colorScheme = (!reverseColorScheme && currentColorScheme)
     || (reverseColorScheme && currentColorScheme === 'light' && 'dark')
     || (reverseColorScheme && currentColorScheme === 'dark' && 'light')
     || null;
 
-  const abbreviations = shorterAbbreviations
-    ? PokemonTypeShortAbbreviations
-    : PokemonTypeAbbreviations;
+  const shouldAbbreviate = ['xs', 'sm'].includes(containerSize);
+
+  // using `null` instead of `-1` to keep TypeScript happy
+  // (even if you say `labelIndex > -1`, TypeScript can't infer that labelIndex must be 0, 1, or 2)
+  const labelIndex = type && type !== '???' && type in PokemonTypeLabels
+    ? shouldAbbreviate
+      ? containerSize === 'sm' ? 1 : 2
+      : 0
+    : null;
+
+  // TypeScript also can't infer that we've previously checked `type !== '???'` in labelIndex
+  // (otherwise it would be `null`), so we gotta check again to keep it happy
+  const label = (
+    typeof labelIndex === 'number'
+      && type !== '???' // this is also necessary for TypeScript lol
+      && PokemonTypeLabels[type]?.[labelIndex]
+  ) || defaultLabel || '???';
 
   return (
     <span
@@ -40,13 +56,13 @@ export const PokeType = ({
         !!colorScheme && styles[colorScheme],
         // (!type || type === '???') && styles['type-unknown'],
         !!type && type !== '???' && styles[`type-${type.toLowerCase()}`],
-        shorterAbbreviations && styles.shorter,
+        shouldAbbreviate && styles[containerSize],
         highlight && styles.highlight,
         className,
       )}
       style={style}
     >
-      {((!type || type === '???') && defaultLabel) || abbreviations[type || '???']}
+      {label}
     </span>
   );
 };
