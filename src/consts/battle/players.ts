4439c2505779feb67a3f07b1224f3387c34e77da
Keith Choison <keith.c@live.co.kr>
2023-01-22 20:05:46 -0800

    feat(consts): added AllPlayerKeys const

---
+++ src/consts/battle/players.ts
@@ -0,0 +1,13 @@
+import type { CalcdexPlayerKey } from '@showdex/redux/store';
+
+/**
+ * All player keys available in the `battle` object.
+ *
+ * @since 1.1.3
+ */
+export const AllPlayerKeys: CalcdexPlayerKey[] = [
+  'p1',
+  'p2',
+  'p3',
+  'p4',
+];
