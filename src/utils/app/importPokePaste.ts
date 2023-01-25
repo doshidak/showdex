8bc7a41025a9419354ec905a27d8d2aa8936c895
Keith Choison <keith.c@live.co.kr>
2023-01-24 19:38:26 -0800

    refac(utils): updated imports in import/exportPokePaste

--- src/utils/app/importPokePaste.ts
+++ src/utils/app/importPokePaste.ts
@@ -1,14 +1,12 @@
 import { PokemonNatures, PokemonNeutralNatures, PokemonTypes } from '@showdex/consts/pokemon';
-import { formatId } from '@showdex/utils/app';
+import { detectGenFromFormat, detectLegacyGen, getDexForFormat } from '@showdex/utils/battle';
 import { calcPresetCalcdexId } from '@showdex/utils/calc';
 import { clamp, env } from '@showdex/utils/core';
 import { capitalize } from '@showdex/utils/humanize';
 import type { GenerationNum } from '@smogon/calc';
 import type { AbilityName, ItemName, MoveName } from '@smogon/calc/dist/data/interface';
 import type { CalcdexPokemonPreset, CalcdexPokemonPresetSource } from '@showdex/redux/store';
-import { detectGenFromFormat } from './detectGenFromFormat';
-import { detectLegacyGen } from './detectLegacyGen';
-import { getDexForFormat } from './getDexForFormat';
+import { formatId } from './formatId';
 
 // note: speciesForme should be handled last since it will test() true against any line technically
 const PokePasteLineParsers: Partial<Record<keyof CalcdexPokemonPreset, RegExp>> = {
