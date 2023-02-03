48466e8c76cbfe6bca6e00c5520e24140074a76c
Keith Choison <keith.c@live.co.kr>
2023-02-02 20:20:13 -0800

    refac(utils): combined read/writeClipboardText into one file

--- src/utils/core/clipboard.ts
+++ src/utils/core/clipboard.ts
@@ -24,3 +24,21 @@ export const readClipboardText = (): Promise<string> => {
 
   return navigator.clipboard.readText();
 };
+
+/**
+ * Browser-agnostic abstraction for `navigator.clipboard.writeText()`.
+ *
+ * * Actually, this is just for completion's sake since `readClipboardText()` exists.
+ *   - Calling the aforementioned function works both on Chrome and Firefox, fortunately.
+ *
+ * @since 1.0.7
+ */
+export const writeClipboardText = (
+  data: string,
+): Promise<void> => {
+  if (typeof navigator === 'undefined') {
+    return;
+  }
+
+  return navigator.clipboard.writeText(data);
+};
