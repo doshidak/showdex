6844eeffd20d089fd0594a8e8957d1a6f9942537
Keith Choison <keith.c@live.co.kr>
2023-04-10 22:20:53 -0700

    feat(hellodex): added user popup support to PatronagePane

--- src/pages/Hellodex/PatronagePane.tsx
+++ src/pages/Hellodex/PatronagePane.tsx
@@ -2,12 +2,18 @@ import * as React from 'react';
 import Svg from 'react-inlinesvg';
 import cx from 'classnames';
 import { BuildInfo } from '@showdex/components/debug';
-import { BaseButton, Scrollable, Tooltip } from '@showdex/components/ui';
+import {
+  BaseButton,
+  Button,
+  Scrollable,
+  Tooltip,
+} from '@showdex/components/ui';
 import { ShowdexDonorTiers, ShowdexPatronTiers } from '@showdex/consts/app';
 import { useAuthUsername, useColorScheme } from '@showdex/redux/store';
-import { findPlayerTitle } from '@showdex/utils/app';
+import { findPlayerTitle, formatId, openUserPopup } from '@showdex/utils/app';
 import { env, getResourceUrl } from '@showdex/utils/core';
 import type { BaseButtonProps } from '@showdex/components/ui';
+import type { ShowdexSupporterTier } from '@showdex/consts/app';
 import type { ElementSizeLabel } from '@showdex/utils/hooks';
 import hellodexStyles from './Hellodex.module.scss';
 import styles from './PatronagePane.module.scss';
@@ -21,6 +27,110 @@ export interface PatronagePaneProps {
 
 const donationUrl = env('hellodex-donation-url');
 const patronageUrl = env('hellodex-patronage-url');
+
+/**
+ * Internally-used tier renderer factory.
+ *
+ * * Return value of the outer function matches the function signature of
+ *   the `callbackFn` argument of `Array.prototype.map()`.
+ *   - This means you can specify this factory as the first argument
+ *     after you specify the `key` (see example).
+ *
+ * @example
+ * ```tsx
+ * <div className={styles.info}>
+ *   {ShowdexDonorTiers.map(TierRenderer('DonorTier'))}
+ * </div>
+ * ```
+ * @since 1.1.5
+ */
+const TierRenderer = (
+  key: string,
+) => (
+  tier: ShowdexSupporterTier,
+  index?: number,
+): JSX.Element => {
+  const {
+    title,
+    names,
+  } = tier || {};
+
+  // note: we're not checking if `names[]` is empty since we render an mdash if it is
+  if (!title || !Array.isArray(names)) {
+    return null;
+  }
+
+  const containerKey = `PatronagePane:${key}:${formatId(title)}`;
+  const notFirstTier = index > 0;
+  const namesCount = names.length;
+
+  return (
+    <React.Fragment key={containerKey}>
+      <div
+        className={styles.heading}
+        style={notFirstTier ? { marginTop: 15 } : undefined}
+      >
+        {title}
+      </div>
+
+      <div
+        className={cx(
+          styles.value,
+          !names.length && styles.empty,
+        )}
+      >
+        {names.length ? names.map((n, i) => {
+          const name = Array.isArray(n) ? n[0] : n;
+
+          if (!name) {
+            return null;
+          }
+
+          const username = formatId(name);
+          const nameKey = `${key}:${username}`;
+
+          const active = !Array.isArray(n) || n[1];
+          const nameStyle: React.CSSProperties = active ? undefined : {
+            opacity: 0.5,
+          };
+
+          const user = Array.isArray(n) && n[2];
+          const notLastChild = i < namesCount - 1;
+
+          return (
+            <React.Fragment key={nameKey}>
+              {user ? (
+                <Button
+                  display="inline"
+                  className={styles.userButton}
+                  labelStyle={nameStyle}
+                  label={name}
+                  tooltip={(
+                    <div className={styles.tooltipContent}>
+                      Open <strong>{name}</strong>'s Profile
+                    </div>
+                  )}
+                  absoluteHover
+                  onPress={() => openUserPopup(name)}
+                />
+              ) : (
+                <span style={nameStyle}>
+                  {name}
+                </span>
+              )}
+              {
+                notLastChild &&
+                <span style={{ opacity: 0.3 }}>
+                  ,{' '}
+                </span>
+              }
+            </React.Fragment>
+          );
+        }) : <>&mdash;</>}
+      </div>
+    </React.Fragment>
+  );
+};
 
 export const PatronagePane = ({
   className,
@@ -141,28 +251,7 @@ export const PatronagePane = ({
                   matching Patreon tier to your donation amount.
                 </div>
 
-                {ShowdexDonorTiers.map((tier, i) => (
-                  <React.Fragment key={`PatronagePane:ShowdexDonorTiers:${tier.title}`}>
-                    <div
-                      className={styles.heading}
-                      style={i > 0 ? { marginTop: 15 } : undefined}
-                    >
-                      {tier.title}
-                    </div>
-
-                    <div
-                      className={styles.value}
-                      style={!tier.names?.length ? {
-                        opacity: 0.3,
-                        userSelect: 'none',
-                      } : undefined}
-                    >
-                      {tier.names?.length ? (
-                        tier.names.map((n) => (Array.isArray(n) ? n[0] : n)).join(', ')
-                      ) : <>&mdash;</>}
-                    </div>
-                  </React.Fragment>
-                ))}
+                {ShowdexDonorTiers.map(TierRenderer('DonorTier'))}
               </div>
 
               <div className={styles.buttonContainer}>
@@ -197,28 +286,7 @@ export const PatronagePane = ({
                   Batteries not included.
                 </div>
 
-                {ShowdexPatronTiers.map((tier, i) => (
-                  <React.Fragment key={`PatronagePane:ShowdexPatronTiers:${tier.title}`}>
-                    <div
-                      className={styles.heading}
-                      style={i > 0 ? { marginTop: 15 } : undefined}
-                    >
-                      {tier.title}
-                    </div>
-
-                    <div
-                      className={styles.value}
-                      style={!tier.names?.length ? {
-                        opacity: 0.3,
-                        userSelect: 'none',
-                      } : undefined}
-                    >
-                      {tier.names?.length ? (
-                        tier.names.map((n) => (Array.isArray(n) ? n[0] : n)).join(', ')
-                      ) : <>&mdash;</>}
-                    </div>
-                  </React.Fragment>
-                ))}
+                {ShowdexPatronTiers.map(TierRenderer('PatronTier'))}
               </div>
 
               <div className={styles.buttonContainer}>
