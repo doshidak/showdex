73eb9d43f91d7b0e36d7a7825050f27555e3685a
Keith Choison <keith.c@live.co.kr>
2023-07-01 10:27:17 -0700

    feat(utils): added hydrateHeader

---
+++ src/utils/hydro/hydrateHeader.ts
@@ -0,0 +1,122 @@
+import { toDate } from 'date-fns';
+import { HydroDescriptor, type HydroHeader } from '@showdex/interfaces/hydro';
+
+/**
+ * Hydrates a string `value` into a `HydroHeader`.
+ *
+ * * Remaining non-header information will be conveniently pre-`split()` via the `delimiter` &
+ *   available in the second element of the returned tuple.
+ *   - Their opcodes will remain in-tact.
+ * * This can conveniently be used to separate the header from the payload (see example below).
+ * * An empty array (i.e., `[]`) will be returned if hydration fails for whatever reason.
+ *
+ * @example
+ * ```ts
+ * hydrateHeader('v:1.1.6;@:showdex-v1.1.6-b188FC84C1A9-dev.chrome;#:18904BAC42E;$:presets;u:...;p:...;p:...');
+ *
+ * [
+ *   {
+ *     version: '1.1.6',
+ *     build: 'showdex-v1.1.6-b188FC84C1A9-dev.chrome',
+ *     timestamp: '18904BAC42E',
+ *     date: Date(),
+ *     descriptor: 'presets',
+ *     descriptorValid: true,
+ *   },
+ *   [
+ *     'u:...',
+ *     'p:...',
+ *     'p:...',
+ *   ]
+ * ]
+ * ```
+ * @since 1.1.6
+ */
+export const hydrateHeader = (
+  value: string,
+  delimiter = ';',
+  opcodeDelimiter = ':',
+): [
+  header?: HydroHeader,
+  remaining?: string[],
+] => {
+  if (!value?.includes(opcodeDelimiter)) {
+    return [];
+  }
+
+  const parts = value.split(delimiter);
+
+  if (!parts?.length) {
+    return [];
+  }
+
+  const header: HydroHeader = {
+    version: null,
+    build: null,
+    timestamp: null,
+    date: null,
+    descriptor: null,
+    descriptorValid: false,
+  };
+
+  const remaining: string[] = [];
+
+  parts.forEach((part) => {
+    if (!part?.includes(opcodeDelimiter)) {
+      return;
+    }
+
+    const [
+      opcode,
+      ...partValueParts
+    ] = part.split(opcodeDelimiter);
+
+    if (!partValueParts.length) {
+      return;
+    }
+
+    const partValue = partValueParts.join(opcodeDelimiter);
+
+    switch (opcode) {
+      case 'v': {
+        header.version = partValue;
+
+        break;
+      }
+
+      case '@': {
+        header.build = partValue;
+
+        break;
+      }
+
+      case '#': {
+        header.timestamp = partValue;
+
+        if (/^[0-9A-F]+$/i.test(partValue)) {
+          header.date = toDate(parseInt(partValue, 16));
+        }
+
+        break;
+      }
+
+      case '$': {
+        header.descriptor = partValue as HydroDescriptor;
+        header.descriptorValid = Object.values(HydroDescriptor).includes(header.descriptor);
+
+        break;
+      }
+
+      default: {
+        remaining.push(part);
+
+        break;
+      }
+    }
+  });
+
+  return [
+    header,
+    remaining,
+  ];
+};
--- src/utils/hydro/index.ts
+++ src/utils/hydro/index.ts
@@ -1,5 +1,7 @@
 export * from './dehydrateCalcdex';
+export * from './dehydrateHeader';
 export * from './dehydratePreset';
 export * from './dehydrators';
+export * from './hydrateHeader';
 export * from './hydratePreset';
 export * from './hydrators';
