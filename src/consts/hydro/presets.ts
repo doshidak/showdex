46203abb2f047c98d77cdac1bf5cbb88e996086a
Keith Choison <keith.c@live.co.kr>
2023-07-03 19:05:32 -0700

    feat(consts): added hydro consts for presets

---
+++ src/consts/hydro/presets.ts
@@ -0,0 +1,100 @@
+import { type CalcdexPokemonPreset } from '@showdex/redux/store';
+import { reverseObjectKv } from '@showdex/utils/core';
+import { type HydroHeader } from './header';
+
+/**
+ * Hydrated object from dehydrated presets.
+ *
+ * @since 1.1.6
+ */
+export interface HydroPresetsHydration extends HydroHeader {
+  /**
+   * Hydrated presets.
+   *
+   * @since 1.1.6
+   */
+  presets?: CalcdexPokemonPreset[];
+}
+
+/* eslint-disable @typescript-eslint/indent */
+
+/**
+ * `CalcdexPokemonPreset` keys that are able to be dehydrated.
+ *
+ * @since 1.1.6
+ */
+export type HydroPresetsDehydrationKeys = Exclude<keyof CalcdexPokemonPreset,
+  | 'id'
+  | 'gen'
+  | 'ability'
+  | 'item'
+  | 'moves'
+>;
+
+/* eslint-enable @typescript-eslint/indent */
+
+/**
+ * Mapping of `CalcdexPokemonPreset` properties to their `string` opcodes.
+ *
+ * @since 1.1.6
+ */
+export const HydroPresetsDehydrationMap: Record<HydroPresetsDehydrationKeys, string> = {
+  calcdexId: 'cid',
+  source: 'src',
+  name: 'nom',
+  playerName: 'pln',
+  format: 'fmt',
+  nickname: 'nkn',
+  usage: 'usg',
+  speciesForme: 'fme',
+  level: 'lvl',
+  gender: 'gdr',
+  hiddenPowerType: 'hpt',
+  teraTypes: 'trt',
+  shiny: 'shy',
+  happiness: 'hpy',
+  dynamaxLevel: 'dml',
+  gigantamax: 'gmx',
+  altAbilities: 'abl',
+  altItems: 'itm',
+  altMoves: 'mov',
+  nature: 'ntr',
+  ivs: 'ivs',
+  evs: 'evs',
+  pokeball: 'pkb',
+};
+
+/**
+ * Mapping of `CalcdexPokemonPreset` properties that are `CalcdexPokemonAlt<T>[]`'s to their non-alt properties.
+ *
+ * * Typically used for dehydrating non-`'smogon'` & non-`'usage'` presets.
+ *   - Alt properties like `altAbilities[]` are typically only populated if derived from the aforementioned sources.
+ *   - Otherwise, we'll only dehydrate the `ability` (unless that's unavailable too LOL -- no biggie tho).
+ * * If there is no mapping (i.e., value is falsy like `null`), then no property override will be performed.
+ *
+ * @since 1.1.6
+ */
+export const HydroPresetsDehydrationAltMap: Partial<Record<HydroPresetsDehydrationKeys, keyof CalcdexPokemonPreset>> = {
+  teraTypes: null,
+  altAbilities: 'ability',
+  altItems: 'item',
+  altMoves: 'moves',
+};
+
+/**
+ * Reverse mapping of `string` opcodes to their `CalcdexPokemonPreset` properties.
+ *
+ * @example
+ * ```ts
+ * HydroPresetsHydrationMap.cid // 'calcdexId'
+ * ```
+ * @since 1.1.6
+ */
+export const HydroPresetsHydrationMap = reverseObjectKv(HydroPresetsDehydrationMap);
+
+/**
+ * lol
+ *
+ * @since 1.1.6
+ */
+export const HydroPresetsDefaultName = 'de_cache';
