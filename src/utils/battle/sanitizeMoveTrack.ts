9411801c7dddead04ef3ece4f42c0efc68d9ad73
Keith Choison <keith.c@live.co.kr>
2023-07-27 20:21:38 -0700

    feat(utils): added transformedMoves to sanitizeMoveTrack

--- src/utils/battle/sanitizeMoveTrack.ts
+++ src/utils/battle/sanitizeMoveTrack.ts
@@ -3,6 +3,35 @@ import { type CalcdexPokemon } from '@showdex/redux/store';
 import { getDexForFormat } from '@showdex/utils/dex';
 
 /**
+ * Internal helper function to `filter()` & `map()` the `moveTrack` to the dex entries via `dex.moves.get()`.
+ *
+ * * If `transformed` is `true`, only moves prepended with an asterisk (i.e., `*`) will be processed.
+ *   - Otherwise, only moves that don't start with an asterisk will be processed (default).
+ *   - Asterisk prefix in the move's name is how Showdown signifies it was inherited after transforming.
+ *
+ * @since 1.1.6
+ */
+const getDexMoveTrack = (
+  dex: Showdown.ModdedDex,
+  moveTrack: CalcdexPokemon['moveTrack'],
+  transformed?: boolean,
+): [
+  move: Showdown.Move,
+  ppUsed: number,
+][] => moveTrack
+  ?.filter((t) => (
+    Array.isArray(t)
+      && typeof t[0] === 'string'
+      && !!t[0]
+      && (transformed ? t[0].startsWith('*') : !t[0].startsWith('*'))
+  ))
+  .map(([moveName, ppUsed]) => [
+    dex.moves.get(moveName?.replace('*', '')),
+    ppUsed || 0,
+  ] as [move: Showdown.Move, ppUsed: number])
+  .filter(([move]) => move?.exists && !!move.name);
+
+/**
  * Sanitizes the `moveTrack` from the passed-in `pokemon` and constructs the `revealedMoves`
  * from the resulting sanitized `moveTrack`, if any.
  *
@@ -13,7 +42,11 @@ import { getDexForFormat } from '@showdex/utils/dex';
  *
  * @default
  * ```ts
- * { moveTrack: [], revealedMoves: [] }
+ * {
+ *   moveTrack: [],
+ *   revealedMoves: [],
+ *   transformedMoves: [],
+ * }
  * ```
  * @since 1.0.3
  */
@@ -27,30 +60,28 @@ export const sanitizeMoveTrack = (
   const output: DeepPartial<CalcdexPokemon> = {
     moveTrack: [],
     revealedMoves: [],
+    transformedMoves: [],
   };
 
   if (!dex || !pokemon?.moveTrack?.length) {
     return output;
   }
 
-  const dexMoveTrack = pokemon.moveTrack
-    .filter((t) => Array.isArray(t) && !!t[0])
-    .map(([moveName, ppUsed]) => [
-      // transformed moves will sometimes have an asterisk (*) in the name
-      dex.moves.get(moveName?.replace('*', '')),
-      ppUsed || 0,
-    ] as [move: Showdown.Move, ppUsed: number])
-    .filter(([move]) => move?.exists && !!move.name);
+  const moveTrack = pokemon.moveTrack as CalcdexPokemon['moveTrack'];
+  const dexMoveTrack = getDexMoveTrack(dex, moveTrack);
+  const dexTransformedMoveTrack = getDexMoveTrack(dex, moveTrack, true);
 
-  if (!dexMoveTrack.length) {
+  if (!dexMoveTrack.length && !dexTransformedMoveTrack.length) {
     return output;
   }
 
-  output.moveTrack = dexMoveTrack
-    .map(([move, ppUsed]) => [
-      move?.name,
-      ppUsed,
-    ] as [moveName: MoveName, ppUsed: number]);
+  output.moveTrack = dexMoveTrack.map(([move, ppUsed]) => [
+    move.name,
+    ppUsed,
+  ] as [moveName: MoveName, ppUsed: number]);
+
+  output.transformedMoves = dexTransformedMoveTrack
+    .map(([move]) => move.name as MoveName);
 
   // filter out any Z/Max moves from the moveTrack
   output.revealedMoves = dexMoveTrack
