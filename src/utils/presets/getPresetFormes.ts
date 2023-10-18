0ee9861b358f7954e3e81bb3e6c8af7b272f446f
Keith Choison <keith.c@live.co.kr>
2023-10-18 14:42:48 -0700

    feat(utils): updated fucked formes handling

--- src/utils/presets/getPresetFormes.ts
+++ src/utils/presets/getPresetFormes.ts
@@ -1,101 +1,197 @@
 import { type GenerationNum } from '@smogon/calc';
-import { PokemonPresetFuckedFormes } from '@showdex/consts/dex';
+import { PokemonPresetFuckedBaseFormes, PokemonPresetFuckedBattleFormes } from '@showdex/consts/dex';
+import { type CalcdexPokemonPresetSource } from '@showdex/redux/store';
 import { formatId } from '@showdex/utils/core';
-import { getDexForFormat } from '@showdex/utils/dex';
+import { getDexForFormat, hasMegaForme } from '@showdex/utils/dex';
 
 /**
- * Returns an array of species formes that `speciesForme`s in `CalcdexPokemonPreset`s should match
- * for the given `speciesForme`.
+ * Returns an array of possible formes for the provided `speciesForme` that the `speciesFormes` of each
+ * `CalcdexPokemonPreset` should match against.
  *
+ * * As of v1.1.7, the `config` object was added to address certain edge cases, particularly the `source` option, which
+ *   if it's `'server'` or `'sheet'`, will return all of the `otherFormes[]` for the provided `speciesForme`.
+ *   - This was done primarily to address mismatching `speciesForme`'s in OTS presets & the Calcdex state, namely for
+ *     a pre-initialized `'Urshifu'` in the state not matching with an OTS preset for `'Urshifu-Rapid-Strike'`.
  * * Guaranteed to at least return an empty array (i.e., `[]`) if building the formes fails at any point.
  *
  * @example
  * ```ts
  * getPresetFormes('Necrozma-Ultra');
  *
- * // hmm... should we return 'Necrozma-Dusk-Mane' as well?
- * // ... probably not, yolo
  * [
  *   'Necrozma-Ultra',
  *   'Necrozma-Dawn-Wings',
+ *   'Necrozma-Dusk-Mane',
  * ]
  * ```
  * @example
  * ```ts
- * // note: formatAsId arg is true here; take note of the output
- * getPresetFormes('Gengar', 'gen8randombattle', true);
+ * getPresetFormes('Gengar', {
+ *   format: 'gen8randombattle',
+ *   formatAsId: true,
+ * });
  *
  * [
  *   'gengar',
  *   'gengargmax',
  * ]
  * ```
+ * @example
+ * ```ts
+ * getPresetFormes('Urshifu');
+ *
+ * [
+ *   'Urshifu',
+ *   'Urshifu-Single-Strike',
+ * ]
+ * ```
+ * @example
+ * ```ts
+ * getPresetFormes('Urshifu', {
+ *   source: 'sheet',
+ * });
+ *
+ * [
+ *   'Urshifu',
+ *   'Urshifu-Single-Strike',
+ *   'Urshifu-Rapid-Strike',
+ * ]
+ * ```
+ * @example
+ * ```ts
+ * getPresetFormes('Ogerpon-Teal-Tera');
+ *
+ * [
+ *   'Ogerpon',
+ *   'Ogerpon-Teal',
+ * ]
+ * ```
  * @since 1.1.2
  */
 export const getPresetFormes = (
   speciesForme: string,
-  format?: GenerationNum | string,
-  formatAsId?: boolean,
+  config?: {
+    format?: string | GenerationNum;
+    source?: CalcdexPokemonPresetSource;
+    formatAsId?: boolean;
+    ignoreMega?: boolean;
+    ignoreGmax?: boolean;
+  },
 ): string[] => {
-  // this will be our final return value
-  const output: string[] = [];
-
   if (!speciesForme) {
-    return output;
+    return [];
   }
+
+  const {
+    format,
+    source,
+    formatAsId,
+    ignoreMega,
+    ignoreGmax,
+  } = config || {};
 
   const dex = getDexForFormat(format);
   const dexSpecies = dex.species.get(speciesForme);
 
-  if (!dexSpecies?.exists) {
-    return output;
+  if (!dexSpecies?.exists || !dexSpecies.name) {
+    return [];
   }
 
-  // e.g., 'Necrozma-Ultra' (typically wouldn't have any sets)
-  output.push(dexSpecies.name);
+  const {
+    name,
+    baseForme,
+    baseSpecies: baseSpeciesForme,
+    battleOnly,
+    changesFrom,
+    otherFormes,
+    isMega,
+    canGigantamax,
+  } = dexSpecies;
 
-  const { baseSpecies: baseForme } = dexSpecies; // e.g., 'Necrozma'
-  const checkBaseForme = !!baseForme && baseForme !== dexSpecies.name;
+  // this will be our final return value
+  const output: string[] = [
+    // e.g., 'Necrozma-Ultra' (typically wouldn't have any sets), 'Urshifu'
+    name,
+  ];
 
-  // Special check for urshifu
-  if (dexSpecies.name === 'Urshifu') {
-    output.push('Urshifu-Rapid-Strike');
+  // throw in any base formes too, why not
+  if (baseForme) {
+    // e.g., name = 'Urshifu', baseForme = 'Single-Strike' -> 'Urshifu-Single-Strike'
+    output.push(`${name}-${baseForme}`);
   }
 
-  const battleFormes = (
-    Array.isArray(dexSpecies.battleOnly)
-      ? dexSpecies.battleOnly // e.g., ['Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane']
-      : [dexSpecies.battleOnly]
-  ).filter(Boolean); // e.g., (for some other Pokemon) 'Darmanitan-Galar' -> ['Darmanitan-Galar']
+  // check for Mega formes
+  if (!ignoreMega && otherFormes?.length) {
+    const megaFormes = otherFormes.filter((f) => hasMegaForme(f));
 
-  if (battleFormes.length) {
-    // e.g., 'Necrozma-Dawn-Wings' (sets would typically match this forme)
-    const fuckedBattleForme = battleFormes.find((f) => PokemonPresetFuckedFormes.includes(f));
-
-    if (fuckedBattleForme) {
-      output.push(fuckedBattleForme);
+    if (megaFormes.length) {
+      output.push(...megaFormes);
     }
-  } else if (checkBaseForme && PokemonPresetFuckedFormes.includes(baseForme)) {
-    // e.g., 'Necrozma' (though for this specific Pokemon, wouldn't apply since it has battleFormes)
-    output.push(baseForme);
+  }
+
+  // update (2023/10/16): separated the fucked formes as to not complicate what goes where in the future lol
+  // e.g., name = 'Ogerpon-Wellspring-Tera' -> includes('Ogerpon-Wellness-Tera') = true
+  // -> battleOnly = 'Ogerpon-Wellspring', changesFrom = undefined
+  if (PokemonPresetFuckedBattleFormes.includes(name) && (battleOnly || changesFrom)) {
+    const battleFormes = (
+      Array.isArray(battleOnly)
+        ? battleOnly // e.g., name = 'Necrozma-Ultra' -> ['Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane']
+        : [battleOnly] // e.g., name = 'Ogerpon-Wellspring' -> ['Ogerpon-Wellspring'] (sorry Darmanitan)
+    ).filter(Boolean);
+
+    if (battleFormes.length) {
+      output.push(...battleFormes);
+    }
+
+    if (changesFrom) {
+      // e.g., name = 'Genesect-Douse' -> battleOnly = undefined, changesFrom = 'Genesect'
+      output.push(changesFrom);
+    }
+  }
+
+  // e.g., name = 'Poltchageist-Artisan', baseSpeciesForme = 'Poltchageist' -> includes('Poltchageist') = true
+  if (name !== baseSpeciesForme && (isMega || PokemonPresetFuckedBaseFormes.includes(baseSpeciesForme))) {
+    output.push(baseSpeciesForme);
   }
 
   // see if we should add the Gmax forme as well, if any
-  if (dexSpecies.canGigantamax) {
-    const dexGigantamax = dex.species.get(`${dexSpecies.name}-Gmax`);
+  if (!ignoreGmax && canGigantamax) {
+    // e.g., name = 'Gengar' -> 'Gengar-Gmax'
+    const dexGigantamax = dex.species.get(`${name}-Gmax`);
 
     if (dexGigantamax?.exists) {
-      // e.g., (for some other Pokemon) 'Gengar-Gmax'
+      // e.g., dexGigantamax.name = 'Gengar-Gmax'
       output.push(dexGigantamax.name);
     }
   }
 
-  // remove duplicate formes with this one simple trick (programmers hate him)
-  const nonDupesOutput = Array.from(new Set(output.filter(Boolean)));
+  // update (2023/10/16): 'server'/'sheet'-sourced presets may have speciesForme discrepancies with what was initially
+  // in the battle (& therefore in the Calcdex state), so if specified, include ALL the possible otherFormes[]
+  // (namely to address a mismatch between an initialized 'Urshifu' & an OTS preset for 'Urshifu-Rapid-Strike')
+  if (['server', 'sheet'].includes(source)) {
+    const allFormes = (
+      name === baseSpeciesForme
+        ? otherFormes // e.g., name = 'Urshifu' -> ['Urshifu-Rapid-Strike']
+        // e.g., name = 'Ogerpon-Wellspring', baseSpeciesForme = 'Ogerpon' -> ['Ogerpon-Wellspring', ...]
+        : dex.species.get(baseSpeciesForme)?.otherFormes
+    ) || [];
 
-  if (formatAsId) {
-    return nonDupesOutput.map((f) => formatId(f));
+    if (name !== baseSpeciesForme) {
+      output.unshift(baseSpeciesForme);
+    }
+
+    if (allFormes.length) {
+      output.push(...allFormes);
+    }
   }
 
-  return nonDupesOutput;
+  // remove duplicate formes with this one simple trick (programmers hate him)
+  // update (2023/10/16): fucc it yolo, save some frames
+  // const nonDupesOutput = Array.from(new Set(output.filter(Boolean)));
+
+  if (formatAsId) {
+    return output.map((f) => formatId(f)).filter(Boolean);
+  }
+
+  return output.filter(Boolean);
 };
