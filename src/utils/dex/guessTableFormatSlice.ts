e3298f91c786c8882851228b30a193aee1e854bb
Keith Choison <keith.c@live.co.kr>
2023-12-31 13:57:22 -0800

    fix(utils): fixed invalid species forme tiers for VGC formats

--- src/utils/dex/guessTableFormatSlice.ts
+++ src/utils/dex/guessTableFormatSlice.ts
@@ -39,6 +39,7 @@ const KnownMappings: Record<string, Showdown.BattleTeambuilderTableFormatCode> =
   ubers: 'Uber',
   ubersuu: 'UU',
   uu: 'UU',
+  vgc: 'Regular',
   zu: 'ZU',
 };
 
@@ -63,6 +64,11 @@ export const guessTableFormatSlice = (
   }
 
   const { base } = parseBattleFormat(format);
+
+  // special case cause I don't want to define 'vgc2009', 'vgc2010', 'vgc2011', ...
+  if (base.startsWith('vgc')) {
+    return KnownMappings.vgc;
+  }
 
   return (
     Object.entries(KnownMappings)
