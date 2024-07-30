6f86c24c79890ed508c5cc77539cb1b20a96ccea
Keith Choison <keith.c@live.co.kr>
2024-07-30 11:51:32 -0700

    fix(utils): fixed unsanitized RegExp in mapAutoBoosts
    which would silently crash the Calcdex within Redux, typically leaving
    it in an uninitialized or hanging state. also just applied it to
    mapStellarMoves(), just in case; there are more instances of RegExp's
    constructed from strings, but we're in control of those so no need to
    escape them

--- src/utils/battle/mapAutoBoosts.ts
+++ src/utils/battle/mapAutoBoosts.ts
@@ -1,4 +1,5 @@
 import { type AbilityName, type GenerationNum, type ItemName } from '@smogon/calc';
+import escape from 'regexp.escape';
 import { PokemonBoostNames } from '@showdex/consts/dex';
 import {
   type CalcdexAutoBoostEffect,
@@ -22,7 +23,7 @@ const boostStep = (step: string) => step?.startsWith('|-boost') || step?.startsW
 const identRegex = (
   playerKey?: CalcdexPlayerKey,
   searchName?: string,
-) => new RegExp(`(${playerKey || 'p\\d'})[a-z]?:\\s*(${searchName || ''}[\\w\\s-]*)`, 'i');
+) => new RegExp(`(${playerKey || 'p\\d'})[a-z]?:\\s*(${escape(searchName || '')}[\\w\\s-]*)`, 'i');
 
 /**
  * Creates a mapping of the `pokemon`'s applied auto-boost effects from the provided `stepQueue[]`.
---
+++ types/regexp.escape.d.ts
@@ -0,0 +1,9 @@
+/**
+ * @file `regexp.escape.d.ts` - Type declarations for `regexp.escape`.
+ * @author Keith Choison <keith@tize.io>
+ * @since 2.0.1
+ */
+
+declare module 'regexp.escape' {
+  export default function escape(string: string): string;
+}
