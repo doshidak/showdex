33271fbefeae1d0f9fdc74ad7a207356682db198
Keith Choison <keith.c@live.co.kr>
2024-07-31 10:30:11 -0700

    chore: fixed another dumb automerge that snuck through
    lmao

--- src/utils/ui/buildFormatOptions.ts
+++ src/utils/ui/buildFormatOptions.ts
@@ -128,7 +128,7 @@ export const buildFormatOptions = (
 
   if (favoritedFormats.length) {
     sections.unshift({
-      label: 'Favorites',
+      label: translateHeader('Favorites'),
       options: favoritedFormats.map((format) => {
         const { base, label } = parseBattleFormat(format);
         const value = getGenfulFormat(gen, base);
@@ -168,7 +168,7 @@ export const buildFormatOptions = (
   }
 
   const otherFormats: CalcdexBattleFormatOption = {
-    label: 'Other',
+    label: translateHeader('Other'),
     options: [],
   };
 
@@ -199,7 +199,7 @@ export const buildFormatOptions = (
 
   if (currentFormatLabel && !filterFormatLabels.includes(currentFormatLabel)) {
     options.unshift({
-      label: 'Current',
+      label: translateHeader('Current'),
       options: [{
         label: currentFormatLabel,
         value: currentFormat,
