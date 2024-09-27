0bee58fa897841f0ee0a2500786db11131f7fe43
Keith Choison <keith.c@live.co.kr>
2024-09-27 15:51:00 -0700

    fix(app): fixed Bakedex bundles corrupting existing cache
    whenever it updates cause it only referenced whatever value was
    previously queued up to be written to the IndexedDB, which could be
    empty depending on what was stale at the time, so our PayPal donors
    would disappear all of a sudden just cause the Patreon peeps got updated
    LOL oopsies

--- src/utils/app/bakeBakedexBundles.ts
+++ src/utils/app/bakeBakedexBundles.ts
@@ -53,12 +53,12 @@ export const bakeBakedexBundles = async (
 
   const { bundled } = await readMetaDb<Record<'bundled', number>>(['bundled'], { db });
   const { buns } = await readBundlesDb(['buns'], { db });
-  const cacheStale = !nonEmptyObject(buns) || (bundled && compareAsc(new Date(), add(new Date(bundled), maxAge)) > -1);
+  const cacheStale = !nonEmptyObject(buns) || !bundled || compareAsc(new Date(), add(new Date(bundled), maxAge)) > -1;
 
-  let latestBunNsps = buns;
+  let latestBuns = buns;
 
   if (cacheStale) {
-    const bunsUrl = enabled ? joinUris(baseUrl, 'buns') : getResourceUrl('buns.json');
+    const bunsUrl = enabled && baseUrl ? joinUris(baseUrl, 'buns') : getResourceUrl('buns.json');
     const bunsResponse = await runtimeFetch<BakedexApiBunsResponse>(bunsUrl, fetchOptions);
     const bunsData = bunsResponse.json();
 
@@ -66,19 +66,20 @@ export const bakeBakedexBundles = async (
       return l.error('Couldn\'t fetch the latest bundle catalog:', bunsData);
     }
 
-    ({ payload: latestBunNsps } = bunsData);
+    ({ payload: latestBuns } = bunsData);
   }
 
-  if (!nonEmptyObject(latestBunNsps)) {
+  if (!nonEmptyObject(latestBuns)) {
     return void l.error(
       'Couldn\'t load any bundle catalog at all :o !!',
-      '\n', 'buns', '(cached)', buns, '\n', '(latest)', latestBunNsps,
+      '\n', 'buns', '(cached)', buns,
+      '\n', 'buns', '(latest)', latestBuns,
     );
   }
 
   const staleBunIds: Partial<Record<BakedexApiBunsNamespace, string[]>> = {};
 
-  for (const [nsp, nspBuns] of Object.entries(latestBunNsps) as Entries<typeof latestBunNsps>) {
+  for (const [nsp, latestNspBuns] of Object.entries(latestBuns) as Entries<typeof latestBuns>) {
     if (!BakedexApiBunsNamespaces.includes(nsp)) {
       continue;
     }
@@ -89,20 +90,40 @@ export const bakeBakedexBundles = async (
 
     const cachedNspBuns = Object.values({ ...buns?.[nsp] }) as BakedexApiBunsAssetBundle[];
 
-    for (const nspBun of Object.values(nspBuns) as typeof cachedNspBuns) {
-      const cachedNspBun = cachedNspBuns.find((b) => !!b?.id && b.id === nspBun?.id);
+    for (const latestNspBun of Object.values(latestNspBuns) as typeof cachedNspBuns) {
+      if (!latestNspBun?.id) {
+        continue;
+      }
 
-      if (!cachedNspBun?.id) {
-        staleBunIds[nsp].push(nspBun.id);
+      const cachedNspBun = cachedNspBuns.find((b) => !!b?.id && b.id === latestNspBun.id);
+
+      if (!cachedNspBun?.updated) {
+        l.debug(
+          'Couldn\'t find the cached bundle namespace metadata; marking', nsp, 'bundle', latestNspBun.id, 'as stale!',
+          '\n', 'cachedNspBun', cachedNspBun,
+          '\n', 'latestNspBun', latestNspBun,
+          '\n', 'buns', '(cached)', buns,
+          '\n', 'buns', '(latest)', latestBuns,
+        );
+
+        staleBunIds[nsp].push(latestNspBun.id);
 
         continue;
       }
 
       const cachedDate = new Date(cachedNspBun.updated).valueOf() || 0;
-      const latestDate = new Date(nspBun.updated).valueOf() || 0;
+      const latestDate = new Date(latestNspBun.updated).valueOf() || 0;
 
       if (latestDate > cachedDate) {
-        staleBunIds[nsp].push(nspBun.id);
+        l.debug(
+          'Latest updated timestamp is after cached timestamp; marking', nsp, 'bundle', latestNspBun.id, 'as stale!',
+          '\n', 'cachedNspBun', '(updated)', cachedDate, cachedNspBun,
+          '\n', 'latestNspBun', '(updated)', latestDate, latestNspBun,
+          '\n', 'buns', '(cached)', buns,
+          '\n', 'buns', '(latest)', latestBuns,
+        );
+
+        staleBunIds[nsp].push(latestNspBun.id);
 
         continue;
       }
@@ -110,28 +131,42 @@ export const bakeBakedexBundles = async (
       // load cached bundle into Redux
       const cached = await readBundlesDb([nsp], { db });
 
-      if (!nonEmptyObject(cached[nsp]?.[nspBun.id])) {
-        staleBunIds[nsp].push(nspBun.id);
+      if (!nonEmptyObject(cached[nsp]?.[latestNspBun.id])) {
+        l.debug(
+          'Actual cached bundle assets don\'t exist apparently; marking', nsp, 'bundle', latestNspBun.id, 'as stale!',
+          '\n', 'cached', cached,
+          '\n', 'cachedNspBun', cachedNspBun,
+          '\n', 'latestNspBun', latestNspBun,
+          '\n', 'buns', '(cached)', buns,
+          '\n', 'buns', '(latest)', latestBuns,
+        );
+
+        staleBunIds[nsp].push(latestNspBun.id);
 
         continue;
       }
 
       // 'presets' aren't being *directly* loaded into Redux btw (i.e., the ShowdexSliceState)
       // (instead, they're loaded into the RTK Query API endpoint slice via buildBundleQuery() from @showdex/redux/factories)
-      if (nspBun.ntt !== 'presets') {
-        (statePayload as Record<typeof nspBun.ntt, unknown[]>)[nspBun.ntt] = [
-          ...(statePayload[nspBun.ntt] || []),
-          ...(Array.isArray(cached[nsp][nspBun.id]) ? (cached[nsp][nspBun.id] as unknown[]).map((item) => ({
+      if (latestNspBun.ntt !== 'presets') {
+        (statePayload as Record<typeof latestNspBun.ntt, unknown[]>)[latestNspBun.ntt] = [
+          ...(statePayload[latestNspBun.ntt] || []),
+          ...(Array.isArray(cached[nsp][latestNspBun.id]) ? (cached[nsp][latestNspBun.id] as unknown[]).map((item) => ({
             ...(item as Record<string, unknown>),
-            __bunId: nspBun.id,
+            __bunId: latestNspBun.id,
             __updated: cachedDate,
           })) : []),
         ];
       }
 
       statePayload.buns = {
+        ...latestBuns,
         ...statePayload.buns,
-        [nsp]: { ...statePayload.buns?.[nsp], [nspBun.id]: { ...nspBun } },
+        [nsp]: {
+          ...latestNspBuns,
+          ...statePayload.buns?.[nsp],
+          [latestNspBun.id]: { ...latestNspBun },
+        },
       };
     }
   }
@@ -140,8 +175,10 @@ export const bakeBakedexBundles = async (
 
   l.debug(
     'Found', hasChanges ? 'some' : 'no', 'bundles to', enabled ? 'update' : 'load',
-    '\n', 'buns', '(cached)', buns, '\n', '(latest)', latestBunNsps,
-    '\n', 'staleBunIds', staleBunIds,
+    '\n', 'buns', '(cached)', buns,
+    '\n', 'buns', '(latest)', latestBuns,
+    '\n', 'staleBunIds[]', staleBunIds,
+    '\n', 'statePayload', statePayload,
   );
 
   if (hasChanges) {
@@ -151,9 +188,9 @@ export const bakeBakedexBundles = async (
       }
 
       for (const id of ids) {
-        const nspBun = latestBunNsps[nsp][id];
+        const nspBun = latestBuns[nsp][id];
         const prebundleUrl = getResourceUrl(`${id}.${nspBun.ext || 'json'}`);
-        const bundleUrl = enabled ? joinUris(baseUrl, nsp, id) : prebundleUrl;
+        const bundleUrl = enabled && baseUrl ? joinUris(baseUrl, nsp, id, nspBun.ext) : prebundleUrl;
 
         let bundleData: BakedexApiBundleResponse<'presets' | 'titles' | 'tiers'> = null;
 
@@ -183,22 +220,21 @@ export const bakeBakedexBundles = async (
         }
 
         (writePayload as Record<typeof nsp, unknown>)[nsp] = {
+          ...latestBuns[nsp],
           ...writePayload[nsp],
           [id]: bundleData.payload,
         };
 
-        const shouldUpdateState = (nsp === 'players' || nsp === 'supporters')
-          && (bundleData.ntt === 'titles' || bundleData.ntt === 'tiers')
+        const shouldUpdateState = ['players', 'supporters'].includes(nsp)
+          && ['titles', 'tiers'].includes(bundleData.ntt)
           && Array.isArray(bundleData.payload);
 
         if (shouldUpdateState) {
           const ntt = bundleData.ntt as 'titles' | 'tiers'; // nt ts
-          const payload = bundleData.payload as typeof statePayload[typeof ntt];
 
-          // ggwp
           (statePayload as Record<typeof ntt, unknown[]>)[ntt] = [
             ...(statePayload[ntt] || []),
-            ...payload.map((item) => ({
+            ...(bundleData.payload as typeof statePayload[typeof ntt]).map((item) => ({
               ...(item as Record<string, unknown>),
               __bunId: id,
               __updated: new Date(nspBun.updated).valueOf(),
@@ -207,8 +243,13 @@ export const bakeBakedexBundles = async (
         }
 
         writePayload.buns = {
+          ...latestBuns,
           ...writePayload.buns,
-          [nsp]: { ...writePayload.buns?.[nsp], [id]: { ...nspBun } },
+          [nsp]: {
+            ...latestBuns[nsp],
+            ...writePayload.buns?.[nsp],
+            [id]: { ...nspBun },
+          },
         };
       }
     }
@@ -221,15 +262,16 @@ export const bakeBakedexBundles = async (
     await writeMetaDb({ bundled: Date.now() }, { db });
 
     statePayload.buns = { ...writePayload.buns };
-  }
 
-  l.debug(
-    'Updated', hasWrites ? 'no' : 'some', 'new bundles',
-    '\n', 'buns', '(cached)', buns, '\n', '(latest)', latestBunNsps,
-    '\n', 'staleBunIds[]', staleBunIds,
-    '\n', 'writePayload', writePayload,
-    '\n', 'statePayload', statePayload,
-  );
+    l.debug(
+      'Updated some new bundles',
+      '\n', 'buns', '(cached)', buns,
+      '\n', 'buns', '(latest)', latestBuns,
+      '\n', 'staleBunIds[]', staleBunIds,
+      '\n', 'writePayload', writePayload,
+      '\n', 'statePayload', statePayload,
+    );
+  }
 
   if (typeof store?.dispatch !== 'function' || !Object.values(statePayload).some((v) => nonEmptyObject(v))) {
     return;
